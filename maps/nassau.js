cbf5fdc0a7bcd09396b406af5df1a99c268429c5
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2010-01-02 03:24:28 +0000

    new village

---
+++ maps/nassau.js
@@ -0,0 +1,56 @@
+mappages["nassau"] = new Object();
+mappages["nassau"].terrain = new Array;
+ mappages["nassau"].terrain[0] = '.. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. ..';
+ mappages["nassau"].terrain[1] = '.. .. .. ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## .. .. .. .. .. ,, .. .. .. .. .. .. ..';
+ mappages["nassau"].terrain[2] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. .. ,, .. .. .. ,, .. .. ,, .. ..';
+ mappages["nassau"].terrain[3] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. .. .. ,, ,, .. .. ,, ,, .. .. ..';
+ mappages["nassau"].terrain[4] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. ,, .. .. .. .. .. .. .. .. .. ..';
+ mappages["nassau"].terrain[5] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. .. .. ,, .. ,, .. .. .. .. .. ..';
+ mappages["nassau"].terrain[6] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. ,, .. .. .. .. .. .. .. ,, .. .. ..';
+ mappages["nassau"].terrain[7] = '.. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. .. ,, ,, .. ,, .. .. .. .. ,, ..';
+ mappages["nassau"].terrain[8] = '.. .. .. ## ## ## ## ## ## ## ## ## ## ## ## ## +D ## ## ,, ,, .. .. .. .. .. ,, .. .. .. .. ..';
+ mappages["nassau"].terrain[9] = '.. ,, .. .. ,, .. .. ,, .. .. ,, .. .. ,, ,, .. .. .. .. ,, .. .. .. .. .. .. .. .. .. .. .. ..';
+mappages["nassau"].terrain[10] = ',, .. ,, .. .. .. .. ,, ,, .. .. ,, ,, .. .. .. .. .. ,, ,, .. ## ## ## ## ## ## ## ## ## ## ,,';
+mappages["nassau"].terrain[11] = '.. .. .. ,, .. .. ,, .. .. .. .. .. .. .. ,, .. .. .. ,, .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ## ,,';
+mappages["nassau"].terrain[12] = '.. ,, ,, ,, .. ,, ,, ,, ,, ,, .. ,, .. ,, ,, .. .. .. ,, ,, ,, ## ++ ++ ++ ++ ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[13] = '.. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[14] = '.. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. +D ++ ++ ++ ++ ++ ++ ++ ++ ## ,,';
+mappages["nassau"].terrain[15] = '.. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. ## ++ ++ ++ ++ ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[16] = ',, .. ,, .. ,, ,, .. ,, .. .. ,, .. .. ,, ,, .. .. .. ,, ,, ,, ## ++ ++ ++ ## ## ## ## ## ## ..';
+mappages["nassau"].terrain[17] = '.. ## ## ## ## ## ## ## ## ## ## .. .. .. ,, .. .. .. ,, ;; ;; ## ++ ++ ++ ## ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[18] = ',, ## ++ ++ ++ ++ ++ ++ ++ ++ ## ,, ,, .. .. .. .. .. .. ;; .. ## ++ ++ ++ ## ++ ++ ++ ++ ## ,,';
+mappages["nassau"].terrain[19] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ ## ,, .. .. ,, .. .. .. ,, .. ;; ## ++ ++ ++ ## ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[20] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. ,, ,, .. .. .. .. ,, .. ## ++ ++ ++ +D ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[21] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ +D .. .. .. ,, .. .. .. ,, .. .. ## ++ ++ ++ ## ## ## ## ## ## ..';
+mappages["nassau"].terrain[22] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. ,, ,, .. .. .. ,, ,, .. ## ++ ++ ++ ## ++ ++ ++ ++ ## ,,';
+mappages["nassau"].terrain[23] = ',, ## ++ ++ ++ ++ ++ ++ ++ ++ ## .. ,, .. ,, .. .. .. .. .. .. ## ++ ++ ++ ## ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[24] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. .. .. .. .. .. ,, .. .. ## ++ ++ ++ ## ++ ++ ++ ++ ## ,,';
+mappages["nassau"].terrain[25] = '.. ## ++ ++ ++ ++ ++ ++ ++ ++ ## .. .. ,, ,, .. .. .. ,, ,, .. ## ++ ++ ++ +D ++ ++ ++ ++ ## ..';
+mappages["nassau"].terrain[26] = '.. ## ## ## ## ## ## ## ## ## ## .. .. ,, .. .. .. .. ;; ,, .. ## ## ## ## ## ## ## ## ## ## ..';
+mappages["nassau"].terrain[27] = '.. .. .. ,, ,, .. .. ,, .. .. .. ;; .. .. ,, .. .. .. ,, .. .. ,, .. ,, ,, .. .. .. .. .. .. ..';
+mappages["nassau"].terrain[28] = '.. ,, ,, ,, .. .. ,, .. ;; .. ,, .. ,, .. ,, .. .. .. ,, ,, .. .. .. .. .. .. ,, .. ,, .. .. ..';
+mappages["nassau"].terrain[29] = ',, .. ,, .. ,, ,, .. ;; ;; .. ,, ,, .. .. ,, .. .. .. .. .. ,, .. .. ,, ,, .. .. ,, .. .. .. ,,';
+mappages["nassau"].terrain[30] = '.. .. ,, .. .. .. ,, .. ,, ,, .. ,, .. ,, ,, .. .. .. ,, .. ;; ,, ,, .. .. ,, .. .. .. ,, .. ..';
+mappages["nassau"].terrain[31] = '.. .. .. ,, .. .. .. .. ,, ,, .. .. .. .. ,, .. .. .. ,, ,, .. ,, .. ;; .. .. .. .. .. .. .. ..';
+
+mappages["nassau"].features = new Array;
+mappages["nassau"].features[0] = {name : 'DoorWindow', x : 10, y : 21};
+mappages["nassau"].features[1] = {name : 'DoorWindow', x : 21, y : 14};
+mappages["nassau"].features[2] = {name : 'Door', x : 25, y : 25};
+mappages["nassau"].features[3] = {name : 'Door', x : 25, y : 20};
+mappages["nassau"].features[4] = {name : 'DoorWindow', x : 16, y : 8};
+mappages["nassau"].features[5] = {name : 'Well', x : 26, y : 4};
+
+
+mappages["nassau"].npcs = new Array;
+
+mappages["nassau"].desc = 'the Village of Nassau';
+mappages["nassau"].music = 'Towne';
+mappages["nassau"].exitmap = 'darkunknown';
+mappages["nassau"].exitx = '68';
+mappages["nassau"].exity = '71';
+mappages["nassau"].wraps = 'None';
+mappages["nassau"].enterx = '25';
+mappages["nassau"].entery = '48';
+mappages["nassau"].seeBelow = '';
+mappages["nassau"].linkedMaps = new Array("");
