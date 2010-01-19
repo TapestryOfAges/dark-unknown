44f6162a697187331661da7908eaaf88fada3586
madmanatw <gf@tapestryofages.com>
2010-01-19 09:07:21 +0000

    variable shuffling

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ var mode = "base";
 // base, conversation, direction choice
 
 var mappages = new Pages();
-
-var worldmap = new GameMap();
-var PC = new PCObject();
-var gamestate = new GameStateData();
 var localFactory = new tileFactory();
 var eidos = new Platonic();
 
+var PC = new PCObject();
+var gamestate = new GameStateData();
+var maps = new MapMemory();
+var worldmap = maps.addMap("darkunknown");
+
+var debug = 0;
+var debugscreen;
 
 
 function drawCharFrame() {
@@ -19,8 +22,13 @@ function drawCharFrame() {
 	$("#charstats").html(txt);
 }
 
-function drawMainFrame() {
-	
+function drawMainFrame(how) {
+  var mapdiv;
+  for (var i=PC.getx - 6; i<= PC.getx + 6; i++ ){
+  	for (var j=PC.gety - 6; j<= PC.gety + 6; j++ ){
+  		
+  	}
+  }	
 }
 
 function drawTopbarFrame(txt) {
@@ -31,4 +39,12 @@ $(document).ready(function() {
 	gamestate.loadGame();
   drawCharFrame();
   drawTopbarFrame("<p>Lands of Olympus</p>");
+  worldmap.loadMap("darkunknown");
+  drawMainFrame("draw");
+
+  
+  $(window).keydown(function(e) {
+   var code = (e.keyCode ? e.keyCode : e.which);
+   // will be a function call to main function
+  });
 });
