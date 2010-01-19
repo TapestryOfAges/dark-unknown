a84b94b6804ba14a009c3f7e9d73aa17a75f5107
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2010-01-19 09:07:04 +0000

    loading game sets PC to start location

--- save.js
+++ save.js
@@ -7,7 +7,9 @@ function GameStateData() {
 
 
 GameStateData.prototype.loadGame = function() {
-	
+	// Temporarily, this will return demo values
+	PC.setx(64);
+	PC.sety(71);
 }
 
 GameStateData.prototype.saveGame = function() {
