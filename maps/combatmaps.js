53d9575445f4cbcdf144c48fe7948cadfc0a95d3
madmanatw <gf@tapestryofages.com>
2012-02-05 07:47:09 +0000

    enter and exittest functions

--- maps/combatmaps.js
+++ maps/combatmaps.js
@@ -31,8 +31,22 @@ mappages["combatGrass1"].seeBelow = '';
 mappages["combatGrass1"].lightLevel = 'bright';
 mappages["combatGrass1"].alwaysRemember = '0';
 mappages["combatGrass1"].scale = '1';
+mappages["combatGrass1"].enterscript = 'set_exits';
+mappages["combatGrass1"].entertestscript = '';
+mappages["combatGrass1"].exitscript = '';
+mappages["combatGrass1"].exittestscript = 'check_escape';
 mappages["combatGrass1"].linkedMaps = new Array("");
 
+// manually added
+
+mappages["combatGrass1"].set_exits = function(mapref) {
+  maps_set_exits(mapref);
+}
+
+
+mappages["combatGrass1"].check_escape = function(mapref) {
+  maps_check_escape(mapref);
+}
 
 mappages["combatGrass2"] = new Object();
 mappages["combatGrass2"].terrain = new Array;
@@ -67,4 +81,37 @@ mappages["combatGrass2"].seeBelow = '';
 mappages["combatGrass2"].lightLevel = 'bright';
 mappages["combatGrass2"].alwaysRemember = '0';
 mappages["combatGrass2"].scale = '1';
+mappages["combatGrass2"].enterscript = 'set_exits';
+mappages["combatGrass2"].entertestscript = '';
+mappages["combatGrass2"].exitscript = '';
+mappages["combatGrass2"].exittestscript = 'check_escape';
 mappages["combatGrass2"].linkedMaps = new Array("");
+
+// manually added
+
+mappages["combatGrass2"].set_exits = function(mapref) {
+  maps_set_exits(mapref);
+}
+
+mappages["combatGrass2"].check_escape = function(mapref) {
+  maps_check_escape(mapref);
+}
+
+
+// for all combat maps
+
+function maps_set_exits(mapref) {
+  mapref.Enter = function(who,frommap,fromx,fromy,tox,toy) {
+    this.setExitToMap(frommap.getName());
+    this.setExitToX(fromx);
+    this.setExitToY(fromy);    
+  }
+}
+
+function maps_check_escape(mapref) {
+  mapref.ExitTest = function(who,frommap,fromx,fromy,tox,toy) {
+    // ADD CODE here for checking for bribery
+    
+    return 1;  // allow to leave, later will depend on bribery
+  }
+}
