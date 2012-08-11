78c9eef53a78f8ad4fcb72218d6a23150a421c1e
madmanatw <gf@tapestryofages.com>
2012-08-11 05:00:54 +0000

    combat refactoring

--- npcutil.js
+++ npcutil.js
@@ -60,12 +60,11 @@ function Attack(atk, def) {
   if (tohit < .05) { tohit = .05; }
   
 //  var preanim = PreAnimationEffect(mapref, fromx,fromy,tox,toy,graphic,xoffset,yoffset,destgraphic,destxoffset,destyoffset)
-  
+  var dmg = 0;
   if (Math.random() <= tohit) {
     // Hit!
-    // animation and sound here!!!
     
-    var dmg = weapon.rollDamage(atk);
+    dmg = weapon.rollDamage(atk);
     var armor = def.getEquipment("armor");
     var absorb = 0;
     if (armor) {
@@ -76,12 +75,6 @@ function Attack(atk, def) {
     dmg = Math.floor(dmg * (1-absorb));
     if (dmg == 0) { dmg = 1; }  // min dmg 1
 
-    var stillalive = def.dealDamage(dmg, atk);    
-    if (stillalive) {
-      var damagedesc = GetDamageDescriptor(def); 
-      retval["txt"] += ": " + damagedesc + "!"; 
-    }
-    else { retval["txt"] += ": Killed!"; }
     
   }
   else { // Miss!
@@ -89,10 +82,41 @@ function Attack(atk, def) {
     retval["txt"] = retval["txt"] + " - missed!";
   }
   
+  // animate attack
+  var fromcoords = getCoords(atk.getHomeMap(),atk.getx(), atk.gety());
+  var tocoords = getCoords(def.getHomeMap(),def.getx(), def.gety());
+
+  // get graphic, xoffset, yoffset for graphic:  TODO
+
+  var tablehtml = '<table id="animtable" style="z-index:4; position: absolute; left: ' + fromcoords.x + 'px; top: ' + fromcoords.y + '><tr><td style="background-image:url(\'graphics/' + graphic + '\',background-repeat:no-repeat; background-position: ' + xoffset + 'px ' + yoffset + 'px;"><img src="graphics/spacer.gif" width="32" height="32"></td></tr></table>';
+  tilecursor.tileid = "#td-tile" + displayspecs.leftedge + "x" + displayspecs.topedge;
+  tilecursor.basetile = $(tilecursor.tileid).html(); 
+  $(tilecursor.tileid).html($tilecursor.tileid.html() + tablehtml);
+  var duration = Math.pow( Math.pow(tox - fromx, 2) + Math.pow (toy - fromy, 2)  , .5);
+  $("#animtable").animate({ left: tocoords.x , top: tocoords.y } , duration, function() {
+  
+  }
+}
+
+function PostAnimation1(mapref, frommob, tomob, dmg, retval) {
+  
+  if (dmg != 0) {
+    var stillalive = def.dealDamage(dmg, atk);    
+    if (stillalive) {
+      var damagedesc = GetDamageDescriptor(def); 
+      retval["txt"] += ": " + damagedesc + "!"; 
+    }
+    else { retval["txt"] += ": Killed!"; }
+  } else {
+    retval["txt"] = retval["txt"] + " - missed!";
+  }
+    
   retval["fin"] = 1;
   retval["input"] = "&gt;";
   return retval;
+  
 }
+
 
 function GetDamageDescriptor(who) {
   var ratio = who.getHP() / who.getMaxHP();
