8ff25d090923ea0ca497527ae1ead868f4e72fe1
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2013-03-14 00:56:37 +0000

    new file

---
+++ ephemeralObjects.js
@@ -0,0 +1,10 @@
+
+function ephemeralObject() {
+
+  this.type = "";
+// types are: spell, buff, debuff, onAttack, onHit, onIsAttacked, onIsHit
+  this.name = "";
+
+}
+ephemeralObject.prototype = new Object;
+
