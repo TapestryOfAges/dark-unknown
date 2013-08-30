5455999f73dd3c4e6792db07f4b6f6f5ef6c3a8e
madmanatw <gf@tapestryofages.com>
2013-08-30 04:53:11 +0000

    commented out unused (?) function

--- npcutil.js
+++ npcutil.js
@@ -1,11 +1,11 @@
 
-function NPCGroup() {
-  this.min = 1;
-  this.max = 1;
-  this.makeup = new Array;
-  this.makeup[0] = { name: "test", min: 1, max: 1 };
-}
-NPCGroup.prototype = new Object;
+//function NPCGroup() {
+//  this.min = 1;
+//  this.max = 1;
+//  this.makeup = new Array;
+//  this.makeup[0] = { name: "test", min: 1, max: 1 };
+//}
+//NPCGroup.prototype = new Object;
 
 function Anchor() {
   this.x
