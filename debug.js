265c6b8422bf9fa1e7f65d02ea28ba43faa9f3f8
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2013-09-13 02:59:19 +0000

    added function to test that networks are all doubly connected

--- debug.js
+++ debug.js
@@ -9,3 +9,20 @@ function ActivateDebug() {
   debugscreen = window.open('','debugscreen');
   dbs = debugscreen.document;
 }
+
+
+function TestNetwork(mapref, network) {
+  var web = mapref.network[network];
+  for (i=0; i<web.length; i++) {
+    var conn = web[i].connections;
+    var loopback = 0;
+    for (j=0; j<conn.length; j++) {
+      var innerconn = conn[j].connections;
+      for (k=0; k<innerconn.length; k++) {
+        if (innerconn[k] == web[i]) { loopback = 1; }
+      }
+    }
+    if (!loopback) { alert(i + " does not loop back."); }
+  }
+  alert("Check complete.");
+}
