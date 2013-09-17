ba03a87a00fb6c3901e7dbb25d4c9d7ee0170a75
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2013-09-17 00:41:20 +0000

    starting

--- ai.js
+++ ai.js
@@ -1,4 +1,20 @@
 
-function seekPC(who, radius) {
-  
+// object to make it easier to construct which function to call without
+// using eval.
+
+var ais = new Object;
+
+ais.Bandit = function(who, radius) {
+  // If there is a radius attached, hunt for the PC first
+  var hunt = ais.HuntPC(who,radius);
+
+  if (hunt) { return 1; }
+
+}
+
+ais.HuntPC = function(who, radius) {
+	// Is the PC within range to be hunted?
+	if (GetDistance(who.getx(), who.gety(), PC.getx(), PC.gety()) > radius) {
+		return 1;  // don't hunt, do whatever else
+	}
 }
