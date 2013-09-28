777f7c648c6d5b33114b8eb239f10bd914634e47
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2013-09-28 01:13:25 +0000

    waffling

--- ai.js
+++ ai.js
@@ -8,13 +8,16 @@ ais.Bandit = function(who, radius) {
   // If there is a radius attached, hunt for the PC first
   var hunt = ais.HuntPC(who,radius);
 
-  if (hunt) { return 1; }
+  if (hunt) { return 1;  }  // already hunted
 
 }
 
 ais.HuntPC = function(who, radius) {
 	// Is the PC within range to be hunted?
 	if (GetDistance(who.getx(), who.gety(), PC.getx(), PC.gety()) > radius) {
-		return 1;  // don't hunt, do whatever else
+		return 0;  // no hunting
 	}
+	
+	
+	
 }
