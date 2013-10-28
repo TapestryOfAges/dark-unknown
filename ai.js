26d4ad536ccb1c2efc3c68fcffb83bb178c6f2be
madmanatw <gf@tapestryofages.com>
2013-10-28 08:03:47 +0000

    follows paths

--- ai.js
+++ ai.js
@@ -5,9 +5,8 @@
 var ais = {};
 
 ais.Bandit = function(who, radius) {
-  var retval = {};
+  var retval = {fin: 1};
   if (debug) { dbs.writeln("<span style='color:orange; font-weight:bold'>AI " + who.getName() + " " + who.getSerial() + " is going.<br />"); }
-  retval["fin"] = 1;
   // First, see if the PC is adjacent and if so, smite.
   if (GetDistance(who.getx(), who.gety(), PC.getx(), PC.gety()) === 1) {
     NPCAttackPCMap(who);
@@ -15,6 +14,37 @@ ais.Bandit = function(who, radius) {
   }
   
   // Next, check and see if there is already a path that has not expired
+  if ((who.getCurrentPath().length > 0) && (who.getTurnsToRecalcDest() > 0)) {
+    var coords = who.getNextStep();
+    if (GetDistance(who.getx(), who.gety(), coords[0], coords[1]) === 1) {  // the next step is only a step away
+      var diffx = coords[0] - who.getx();
+      var diffy = coords[1] - who.gety();
+      retval = who.moveMe(diffx, diffy, 0);
+      if (retval["canmove"] === 1) { // it moved!
+        return retval; // we're done here
+      }
+      // failed to move. On the surface, this means there was another AI there.
+      // in scale map, could be a closed door.
+      
+      // if there is another AI in the way, randomwalk
+      if (diffx === 1) { retval = ais.Randomwalk(who,20,0,20,20); }
+      else if (diffx === -1) { retval = ais.Randomwalk(who,20,20,20,0); }
+      else if (diffy === 1) { retval = ais.Randomwalk(who,0,20,20,20); }
+      else if (diffy === -1) { retval =  ais.Randomwalk(who,20,20,0,20); }
+      else { alert("How did I get here? ais.Bandit."); }
+      
+      if (retval["nomove"]) { 
+        //didn't move. Not going to try again. Ending turn.
+        retval["fin"] = 1;
+        return retval;
+      } else {
+        // moved. Ending turn.
+        return retval;
+      }
+      
+    } 
+    // if next step is more than one step away, a previous move failed, recalculate now
+  }
   
   // If there is a radius attached, hunt for the PC next
   var hunt = ais.HuntPC(who,radius);
@@ -50,6 +80,33 @@ ais.HuntPC = function(who, radius) {
 	
 }
 
+ais.Randomwalk = function(who, chance_north, chance_east, chance_south, chance_west) {
+  // default values, 25 25 25 25. If it doesn't sum to 100, the remainder is chance_none.
+  var retval = {};
+  var diffx = 0;
+  var diffy = 0;
+  
+  if (chance_north + chance_west + chance_east + chance_south  > 100) {
+    chance_north = 25;
+    chance_south = 25;
+    chance_east = 25; 
+    chance_west = 25;
+  }
+  
+  var roll = Math.floor(Math.random() * 100)+ 1;
+  if (roll <= chance_north) { diffy = -1; }
+  else if (roll - chance_north < chance_east) { diffx = 1; }
+  else if (roll - chance_north - chance_east < chance_south) { diffy = 1; }
+  else if (roll - chance_north - chance_east - chance_south < chance_west) { diffx = -1; }
+  
+  if (diffx === diffy) {  // which at this point can only happen if we aren't moving
+    retval["nomove"] = 1;
+    return retval;
+  }
+  
+  retval = who.moveMe(diffx,diffy);
+  return retval;
+}
 
 function NPCAttackPCMap(npc) {
   var combatmapname = GetCombatMap(npc, PC);
@@ -71,3 +128,4 @@ function NPCAttackPCMap(npc) {
   
   return 1;
 }
+
