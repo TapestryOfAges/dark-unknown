a259c05169a4271c802ada869b1d2ac4b26567a5
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2013-10-29 23:31:58 +0000

    pulled out as a function

--- ai.js
+++ ai.js
@@ -14,37 +14,8 @@ ais.Bandit = function(who, radius) {
   }
   
   // Next, check and see if there is already a path that has not expired
-  if ((who.getCurrentPath().length > 0) && (who.getTurnsToRecalcDest() > 0)) {
-    var coords = who.getNextStep();
-    if (GetDistance(who.getx(), who.gety(), coords[0], coords[1]) === 1) {  // the next step is only a step away
-      var diffx = coords[0] - who.getx();
-      var diffy = coords[1] - who.gety();
-      retval = who.moveMe(diffx, diffy, 0);
-      if (retval["canmove"] === 1) { // it moved!
-        return retval; // we're done here
-      }
-      // failed to move. On the surface, this means there was another AI there.
-      // in scale map, could be a closed door.
-      
-      // if there is another AI in the way, randomwalk
-      if (diffx === 1) { retval = ais.Randomwalk(who,20,0,20,20); }
-      else if (diffx === -1) { retval = ais.Randomwalk(who,20,20,20,0); }
-      else if (diffy === 1) { retval = ais.Randomwalk(who,0,20,20,20); }
-      else if (diffy === -1) { retval =  ais.Randomwalk(who,20,20,0,20); }
-      else { alert("How did I get here? ais.Bandit."); }
-      
-      if (retval["nomove"]) { 
-        //didn't move. Not going to try again. Ending turn.
-        retval["fin"] = 1;
-        return retval;
-      } else {
-        // moved. Ending turn.
-        return retval;
-      }
-      
-    } 
-    // if next step is more than one step away, a previous move failed, recalculate now
-  }
+  retval = ais.SurfaceFollowPath(who,40,1);   
+  if (retval["fin"] === 1) { return retval; }
   
   // If there is a radius attached, hunt for the PC next
   var hunt = ais.HuntPC(who,radius);
@@ -80,9 +51,45 @@ ais.HuntPC = function(who, radius) {
 	
 }
 
+ais.SurfaceFollowPath = function(who, random_nomove, random_tries) {
+  var retval = { fin: 0 };
+  if ((who.getCurrentPath().length > 0) && (who.getTurnsToRecalcDest() > 0)) {
+    var coords = who.getNextStep();
+    if (GetDistance(who.getx(), who.gety(), coords[0], coords[1]) === 1) {  // the next step is only a step away
+      var diffx = coords[0] - who.getx();
+      var diffy = coords[1] - who.gety();
+      retval = who.moveMe(diffx, diffy, 0);
+      if (retval["canmove"] === 1) { // it moved!
+        return retval; // we're done here
+      }
+      // failed to move. On the surface, this means there was another AI there.
+      // in scale map, could be a closed door.
+      
+      // if there is another AI in the way, randomwalk
+      if (!random_tries) { random_tries = 1; }
+      for (var i = 0; i<random_tries; i++) {
+        var split_move = (100-random_nomove)/3;
+        if (diffx === 1) { retval = ais.Randomwalk(who,split_move,0,split_move,split_move); }
+        else if (diffx === -1) { retval = ais.Randomwalk(who,split_move,split_move,split_move,0); }
+        else if (diffy === 1) { retval = ais.Randomwalk(who,0,split_move,split_move,split_move); }
+        else if (diffy === -1) { retval =  ais.Randomwalk(who,split_move,split_move,0,split_move); }
+        else { alert("How did I get here? ais.FollowPath."); }
+        if (!retval["nomove"]) { return retval; }  // moved
+      }
+      
+      //didn't move. Not going to try again. Ending turn.
+      retval["fin"] = 1;
+      return retval;
+      
+    } 
+    // if next step is more than one step away, a previous move failed, recalculate now
+  }
+  return retval;
+}
+
 ais.Randomwalk = function(who, chance_north, chance_east, chance_south, chance_west) {
   // default values, 25 25 25 25. If it doesn't sum to 100, the remainder is chance_none.
-  var retval = {};
+  var retval = { nomove: 0 };
   var diffx = 0;
   var diffy = 0;
   
