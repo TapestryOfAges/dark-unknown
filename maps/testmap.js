9cfaa9ef9da22b724cc9db85fad9730b66d91b0c
madmanatw <gf@tapestryofages.com>
2014-01-21 09:33:07 +0000

    new map for testing LOS problems

---
+++ maps/testmap.js
@@ -0,0 +1,73 @@
+mappages["testmap"] = {};
+mappages["testmap"].terrain = [];
+ mappages["testmap"].terrain[0] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[1] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[2] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[3] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[4] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[5] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+ mappages["testmap"].terrain[6] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK ## ## ## ## ## ## ## ## ## BK BK BK BK BK';
+ mappages["testmap"].terrain[7] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+ mappages["testmap"].terrain[8] = 'BK BK BK BK BK ## ## ## ## ## ## ## ## ## BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK BK';
+ mappages["testmap"].terrain[9] = 'BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[10] = 'BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK ## ++ ++ ++ ## ## ## ## ## BK BK BK BK BK';
+mappages["testmap"].terrain[11] = 'BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK ## ++ ++ ++ ## ## ## ## ## BK BK BK BK BK';
+mappages["testmap"].terrain[12] = 'BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK ## ## ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[13] = 'BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ## BK BK ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[14] = 'BK BK BK BK BK ## ## ## ## ++ ## ## ## ## BK BK ## ++ ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[15] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ## BK BK BK BK ## ++ ## ++ ++ ++ ## ## ## ## ## BK BK BK BK BK';
+mappages["testmap"].terrain[16] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ## BK BK BK BK ## ++ ## ++ ++ ++ ## ## ## ## ## BK BK BK BK BK';
+mappages["testmap"].terrain[17] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ## BK BK BK BK ## ++ ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[18] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ## BK BK BK BK ## ++ ## ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[19] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ## BK BK BK BK ## ++ ## ++ ++ ++ ## ++ ++ ++ ## BK BK BK BK BK';
+mappages["testmap"].terrain[20] = 'BK BK BK BK BK BK BK ## ## ++ ## ## ## BK BK BK ## ++ ## ## ## ## ## ## ## ## ## BK BK BK BK BK';
+mappages["testmap"].terrain[21] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ++ ## BK BK BK ## ++ ## ## ## ## ## ## ## ## BK BK BK BK BK BK';
+mappages["testmap"].terrain[22] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ++ ## ## ## ## ## ^^ ## ## ## ## ++ ++ ++ ## BK BK BK BK BK BK';
+mappages["testmap"].terrain[23] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ ## BK BK BK BK BK BK';
+mappages["testmap"].terrain[24] = 'BK BK BK BK BK BK BK ## ++ ++ ++ ++ ## ## ## ## ## ## ## ## ## ## ++ ++ ++ ## BK BK BK BK BK BK';
+mappages["testmap"].terrain[25] = 'BK BK BK BK BK BK BK ## ## ## ## ## ## BK BK BK BK BK BK BK BK ## ## ## ## ## BK BK BK BK BK BK';
+mappages["testmap"].terrain[26] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["testmap"].terrain[27] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["testmap"].terrain[28] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["testmap"].terrain[29] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["testmap"].terrain[30] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["testmap"].terrain[31] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+
+mappages["testmap"].features = [];
+
+mappages["testmap"].npcs = [];
+
+mappages["testmap"].desc = 'Test Map';
+mappages["testmap"].music = 'Olympus';
+mappages["testmap"].exitmap = 'olympus1';
+mappages["testmap"].exitx = 'undefined';
+mappages["testmap"].exity = 'undefined';
+mappages["testmap"].wraps = 'None';
+mappages["testmap"].enterx = 'undefined';
+mappages["testmap"].entery = 'undefined';
+mappages["testmap"].seeBelow = '';
+mappages["testmap"].lightLevel = 'dark';
+mappages["testmap"].alwaysRemember = '';
+mappages["testmap"].scale = '1';
+mappages["testmap"].enterscript = '';
+mappages["testmap"].entertestscript = '';
+mappages["testmap"].exitscript = '';
+mappages["testmap"].exittestscript = '';
+mappages["testmap"].linkedMaps = [];
+
+
+function RunTest() {
+  debug = 1;
+  ActivateDebug();
+  //PC.setLight(0);
+  var testmap = new GameMap();
+  testmap.loadMap("testmap");
+  maps.addMapByRef(testmap);
+  
+  MoveBetweenMaps(PC, PC.getHomeMap(), testmap, 9, 23);
+  
+  var torch = localFactory.createTile("TorchEast");
+  testmap.placeThing(11,22,torch);
+  DrawMainFrame("draw", PC.getHomeMap().getName() , PC.getx(), PC.gety());
+  
+}
