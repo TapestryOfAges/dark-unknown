2c0e536320672140be85b50805954e62a688027a
madmanatw <gf@tapestryofages.com>
2014-02-13 08:08:02 +0000

    new file

---
+++ merchants.js
@@ -0,0 +1,19 @@
+
+function setMerchants() {
+  var bill = {};
+  bill.low_wpns = [ { item: "Dagger", quantity: 99, price: 5 },
+                    { item: "Shortsword", quantity: 99, price: 45 },
+                    { item: "Mace", quantity: 99, price: 200 },
+                    { item: "Axe", quantity: 99, price: 500},
+                    { item: "Longsword", quantity: 0, price: 1700}, 
+                    { item: "Halberd", quantity: 0, price: 3500}, ];
+  
+  bill.all_wpns = [ { item: "Dagger", quantity: 99, price: 10 },
+                    { item: "Shortsword", quantity: 99, price: 60 },
+                    { item: "Mace", quantity: 99, price: 240 },
+                    { item: "Axe", quantity: 99, price: 560},
+                    { item: "Longsword", quantity: 99, price: 2000}, 
+                    { item: "Halberd", quantity: 99, price: 4200},
+                  ];
+                  
+}
