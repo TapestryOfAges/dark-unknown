e4a05d0bc10a5f13a939dec2b044c3575a2384a0
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2014-02-18 09:25:27 +0000

    added some more bills of sale

--- merchants.js
+++ merchants.js
@@ -17,6 +17,11 @@ function setMerchants() {
                     { item: "Halberd", quantity: 99, price: 4200},
                     { item: "MagicSword", quantity: 0, price: 6600}, 
                   ];
+  bill.poverty_weapons = [ { item: "Dagger", quantity: 99, price: 5 },
+                           { item: "Shortsword", quantity: 99, price: 30 },
+                           { item: "Mace", quantity: 99, price: 160 },
+                           { item: "Sling", quantity: 99, price: 35 },
+                   ];                  
   bill.low_missile = [ { item: "Sling", quantity: 99, price: 50 }, 
                        { item: "Bow", quantity: 99, price: 500 },
                        { item: "Crossbow", quantity: 0, price: 1000 },
@@ -29,5 +34,21 @@ function setMerchants() {
                        { item: "Wand", quantity: 0, price: 5000 },
                        { item: "MagicAxe", quantity: 0, price: 7500 },
                   ];
-                  
+
+
+  bill.alexis = [ { item: "DisarmTrapSpell", quantity: 99, price: 100} ,
+                  { item: "DistractSpell", quantity: 99, price: 100},
+                  { item: "FlameBladeSpell", quantity: 99, price: 100},
+                  { item: "StrikeSpell", quantity: 99, price: 100},
+                  { item: "LesserHealSpell", quantity: 99, price: 200},
+                  { item: "MagicBoltSpell", quantity: 99, price: 200},
+                  { item: "ProtectSpell", quantity: 99, price: 200},
+                  { item: "FireArmorSpell", quantity: 99, price: 400},
+                ];
+  bill.ivan = [ { item: "LesserHealSpell", quantity: 99, price: 150},
+                { item: "ProtectSpell", quantity: 99, price: 150},
+                { item: "IllusionSpell", quantity: 99, price: 150},
+                { item: "TelekinesisSpell", quantity: 99, price: 330},
+                { item: "HealSpell", quantity: 99, price: 700},
+              ];
 }
