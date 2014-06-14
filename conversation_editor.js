6ebb7cbd0f06ff2f4f6fba1a3f4b5945d3eae9ea
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2014-06-14 07:30:24 +0000

    editor progress

--- conversation_editor.js
+++ conversation_editor.js
@@ -1,5 +1,4 @@
 
-var conversations = {};  // directory of Conversations
 var places = {};
 
 var curr_place = "";
@@ -36,5 +35,23 @@ function select_place(pname) {
 }
 
 function select_conv() {
+  var thisconv = document.convform.pickconv.value;
+
+  var txt = "<div style='margin-10'><form name='speechform'><table cellpadding='2' cellspacing='0' border='1'>";
+  txt = txt + "<tr><td>_start</td><td style='color:blue'>[no flag]</td><td>" + conversations[thisconv]["_start"].responses[0] + "</td><td><a href='javascript:edit_response(\"_start\",0)'>Edit</a></td></tr>";
+  if (conversations[thisconv]["_start"].flags) {
+    var keytype;
+    var flag;
+    $.each(conversations[thisconv]["_start"].flags, function(idx,val) {
+      keytype = idx;
+      flag = val;
+    });
+    txt = txt + "<tr><td></td><td style='color:blue'>"+ keytype + " : " + flag + "</td><td>" + conversations[thisconv]["_start"].responses[1] + "</td><td><a href='javascript:edit_response(\"_start\",1)'>Edit</a></td></tr>";
+  } else {
+    txt = txt + "<tr><td></td><td></td><td></td><td><a href='javascript:edit_response(\"_start\",1)'>Edit</a></td></tr>";
+  }
   
+  
+  txt = txt + "</table></form></div>";
+  $(mainbody).html(txt);
 }
