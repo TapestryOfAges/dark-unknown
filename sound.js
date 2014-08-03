81904cc0d8c190284ccdd261def1d237314f458d
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2014-08-03 19:30:38 +0000

    sounds, search

--- sound.js
+++ sound.js
@@ -44,12 +44,20 @@ function stop_music() {
   document.getElementById(nowplaying).currentTime = 0;
 }
 
-function play_footstep() {
+function play_footstep(onwhat) {
   if (laststep === "left") {
-    play_audio("sfx_walk_right");
+    if ((onwhat === "Ocean") || (onwhat === "Water") || (onwhat === "Shallows") || (onwhat === "River")) {
+      play_audio("sfx_walk_water_right");
+    } else {
+      play_audio("sfx_walk_right");
+    }
     laststep = "right";
   } else {
-    play_audio("sfx_walk_left");
+    if ((onwhat === "Ocean") || (onwhat === "Water") || (onwhat === "Shallows") || (onwhat === "River")) {
+      play_audio("sfx_walk_water_left");
+    } else {
+      play_audio("sfx_walk_left");
+    }
     laststep = "left";
   }
 }
