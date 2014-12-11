623b2967d7328ce92e435683d34c956fe1a3fd74
madmanatw <gf@tapestryofages.com>
2014-12-11 08:03:59 +0000

    barely a map as of yet, but a proof of concept

---
+++ maps/ether.js
@@ -0,0 +1,53 @@
+mappages["ether"] = {};
+mappages["ether"].terrain = [];
+ mappages["ether"].terrain[0] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[1] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[2] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[3] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[4] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[5] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[6] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[7] = 'wb wb wb wb wb wb df df df df df wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[8] = 'wb wb wb wb wb wb df df df df df wb wb wb wb wb wb wb wb wb';
+ mappages["ether"].terrain[9] = 'wb wb wb wb wb wb df df df df df wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[10] = 'wb wb wb wb wb wb df df df df df wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[11] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[12] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[13] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[14] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[15] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[16] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[17] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[18] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+mappages["ether"].terrain[19] = 'wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb wb';
+
+mappages["ether"].features = [];
+
+
+mappages["ether"].npcs = [];
+
+mappages["ether"].desc = "Ethereal Plane";
+mappages["ether"].music = 'Magic';
+mappages["ether"].exitmap = '';
+mappages["ether"].exitx = '65';
+mappages["ether"].exity = '70';
+mappages["ether"].wraps = 'None';
+mappages["ether"].enterx = '65';
+mappages["ether"].entery = '70';
+mappages["ether"].seeBelow = '';
+mappages["ether"].lightLevel = 'bright';
+mappages["ether"].alwaysRemember = '0';
+mappages["ether"].scale = '1';
+mappages["ether"].enterscript = '';
+mappages["ether"].entertestscript = '';
+mappages["ether"].exitscript = '';
+mappages["ether"].exittestscript = '';
+mappages["ether"].returnmap = '';
+mappages["ether"].returnx = '';
+mappages["ether"].returny = '';
+mappages["ether"].returninfused = '0';
+mappages["ether"].linkedMaps = [""];
+
+mappages["ether"].onload = function(mapref){ 
+  mapref.worldlayer = "ether.gif";
+}
