b800574c3cbdd51ae610a1971ce4da57dd3350a9
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2015-04-13 08:25:29 +0000

    pretty much finished level 1

--- maps/pitdespair.js
+++ maps/pitdespair.js
@@ -10,14 +10,14 @@ mappages["pitdespair1"].terrain = [];
  mappages["pitdespair1"].terrain[7] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! -= -= -= -= -= -= -= -= -= -= -= -= -= -= ## -= -= -= ## BK BK BK BK BK BK BK BK';
  mappages["pitdespair1"].terrain[8] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! -= -= -= -= -= -= -= -= -= -= -= -= -= -= ## -= -= -= ## BK BK BK BK BK BK BK BK';
  mappages["pitdespair1"].terrain[9] = 'BK BK BK BK BK BK BK ## -= -= -= -= !! !! !! ## ## ## ## ## -= ## ## ## ## ## !! !! !! -= -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[10] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[11] = 'BK BK BK BK BK BK BK ## ## ## ## ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## ## ## ## ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[12] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[13] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[14] = 'BK BK BK BK BK BK BK ## -= -= -= -= !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! -= -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[15] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[16] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[17] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= -= -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[10] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[11] = 'BK BK BK BK BK BK BK ## ## ## ## ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## ## ## ## ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[12] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[13] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[14] = 'BK BK BK BK BK BK BK ## -= -= -= -= !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! -= -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[15] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[16] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[17] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= ## -= -= -= -= -= ## -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
 mappages["pitdespair1"].terrain[18] = 'BK BK BK BK BK BK BK ## ## ## ## ## !! !! !! ## ## ## ## ## ## ## ## ## ## ## !! !! !! ## ## ## ## ## BK BK BK BK BK BK BK BK';
 mappages["pitdespair1"].terrain[19] = 'BK BK BK BK BK BK BK ## -= -= -= ## !! !! !! ## -= -= -= ## -= -= -= -= -= ## !! !! !! ## -= -= -= ## BK BK BK BK BK BK BK BK';
 mappages["pitdespair1"].terrain[20] = 'BK BK BK BK BK BK BK ## -= -= -= -= !! !! !! -= -= -= -= ## -= -= -= -= -= -= !! !! !! -= -= -= -= ## BK BK BK BK BK BK BK BK';
@@ -42,11 +42,11 @@ mappages["pitdespair1"].terrain[38] = 'BK BK BK BK BK BK BK ## -= -= -= ## -= -=
 mappages["pitdespair1"].terrain[39] = 'BK BK BK BK BK BK BK ## -= -= -= -= -= -= -= -= -= ## -= -= -= -= -= -= -= ## -= -= -= -= -= -= -= ## BK BK BK BK BK BK BK BK';
 mappages["pitdespair1"].terrain[40] = 'BK BK BK BK BK BK BK ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## BK BK BK BK BK BK BK BK';
 mappages["pitdespair1"].terrain[41] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[42] = '## BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[43] = '## BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[44] = '## BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[45] = '## BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
-mappages["pitdespair1"].terrain[46] = '## BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[42] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[43] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[44] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[45] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
+mappages["pitdespair1"].terrain[46] = 'BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK BK';
 
 mappages["pitdespair1"].features = [];
 mappages["pitdespair1"].features[0] = {name : 'LadderUp', x : 8, y : 19, entermap : 'darkunknown', enterx : 117, entery : 76};
@@ -64,34 +64,77 @@ mappages["pitdespair1"].features[11] = {name : 'WallPortcullis', x : 25, y : 29,
 mappages["pitdespair1"].features[12] = {name : 'WallPortcullis', x : 29, y : 20, locked : 1};
 mappages["pitdespair1"].features[13] = {name : 'WallPortcullis', x : 29, y : 9, locked : 1};
 mappages["pitdespair1"].features[14] = {name : 'WallPortcullis', x : 29, y : 14, locked : 1};
-mappages["pitdespair1"].features[15] = {name : 'WallDoorway', x : 29, y : 29};
-mappages["pitdespair1"].features[16] = {name : 'WallDoorway', x : 21, y : 36};
-mappages["pitdespair1"].features[17] = {name : 'WallDoorway', x : 11, y : 24};
-mappages["pitdespair1"].features[18] = {name : 'WallDoorway', x : 25, y : 20};
-mappages["pitdespair1"].features[19] = {name : 'WallDoorway', x : 15, y : 24};
-mappages["pitdespair1"].features[20] = {name : 'WallDoorway', x : 15, y : 28};
-mappages["pitdespair1"].features[21] = {name : 'LeverOff', x : 16, y : 13};
-mappages["pitdespair1"].features[22] = {name : 'LeverOff', x : 16, y : 15};
-mappages["pitdespair1"].features[23] = {name : 'LeverOff', x : 16, y : 16};
-mappages["pitdespair1"].features[24] = {name : 'LeverOff', x : 20, y : 17};
-mappages["pitdespair1"].features[25] = {name : 'LeverOff', x : 22, y : 17};
-mappages["pitdespair1"].features[26] = {name : 'LeverOff', x : 18, y : 11};
-mappages["pitdespair1"].features[27] = {name : 'LeverOff', x : 18, y : 13};
-mappages["pitdespair1"].features[28] = {name : 'LeverOff', x : 18, y : 15};
-mappages["pitdespair1"].features[29] = {name : 'LeverOff', x : 24, y : 10};
-mappages["pitdespair1"].features[30] = {name : 'LeverOff', x : 24, y : 12};
-mappages["pitdespair1"].features[31] = {name : 'LeverOff', x : 24, y : 13};
-mappages["pitdespair1"].features[32] = {name : 'LeverOff', x : 24, y : 15};
-mappages["pitdespair1"].features[33] = {name : 'LeverOff', x : 24, y : 16};
-mappages["pitdespair1"].features[34] = {name : 'LeverOff', x : 22, y : 15};
-mappages["pitdespair1"].features[35] = {name : 'LeverOff', x : 22, y : 13};
-mappages["pitdespair1"].features[36] = {name : 'LeverOff', x : 22, y : 11};
-mappages["pitdespair1"].features[37] = {name : 'LeverOff', x : 16, y : 12};
-mappages["pitdespair1"].features[38] = {name : 'Door', x : 14, y : 36, desc : "locked door", locked : 1};
-mappages["pitdespair1"].features[39] = {name : 'Doorway', x : 11, y : 39};
-
+mappages["pitdespair1"].features[15] = {name : 'WallPortcullis', x : 29, y : 29};
+mappages["pitdespair1"].features[16] = {name : 'WallPortcullis', x : 21, y : 36};
+mappages["pitdespair1"].features[17] = {name : 'WallPortcullis', x : 11, y : 24};
+mappages["pitdespair1"].features[18] = {name : 'WallPortcullis', x : 25, y : 20};
+mappages["pitdespair1"].features[19] = {name : 'WallPortcullis', x : 15, y : 24};
+mappages["pitdespair1"].features[20] = {name : 'WallPortcullis', x : 15, y : 28};
+mappages["pitdespair1"].features[21] = {name : 'PitDespairLever', x : 18, y : 14};
+mappages["pitdespair1"].features[22] = {name : 'PitDespairLever', x : 18, y : 16};
+mappages["pitdespair1"].features[23] = {name : 'PitDespairLever', x : 20, y : 17};
+mappages["pitdespair1"].features[24] = {name : 'PitDespairLever', x : 22, y : 17};
+mappages["pitdespair1"].features[25] = {name : 'PitDespairLever', x : 18, y : 11};
+mappages["pitdespair1"].features[26] = {name : 'PitDespairLever', x : 18, y : 13};
+mappages["pitdespair1"].features[27] = {name : 'PitDespairLever', x : 18, y : 15};
+mappages["pitdespair1"].features[28] = {name : 'PitDespairLever', x : 22, y : 10};
+mappages["pitdespair1"].features[29] = {name : 'PitDespairLever', x : 22, y : 12};
+mappages["pitdespair1"].features[30] = {name : 'PitDespairLever', x : 22, y : 14};
+mappages["pitdespair1"].features[31] = {name : 'PitDespairLever', x : 18, y : 17};
+mappages["pitdespair1"].features[32] = {name : 'PitDespairLever', x : 22, y : 16};
+mappages["pitdespair1"].features[33] = {name : 'PitDespairLever', x : 22, y : 15};
+mappages["pitdespair1"].features[34] = {name : 'PitDespairLever', x : 22, y : 13};
+mappages["pitdespair1"].features[35] = {name : 'PitDespairLever', x : 22, y : 11};
+mappages["pitdespair1"].features[36] = {name : 'PitDespairLever', x : 18, y : 12};
+mappages["pitdespair1"].features[37] = {name : 'Door', x : 14, y : 36, desc : "locked door", locked : 1};
+mappages["pitdespair1"].features[38] = {name : 'Doorway', x : 11, y : 39};
+mappages["pitdespair1"].features[39] = {name : 'ExecutionersHood', x : 20, y : 19};
+mappages["pitdespair1"].features[40] = {name : 'Chest', x : 8, y : 36, lootgroup : '', lootedid : ''};
+mappages["pitdespair1"].features[41] = {name : 'Chest', x : 9, y : 36, lootgroup : '', lootedid : ''};
+mappages["pitdespair1"].features[42] = {name : 'Chest', x : 10, y : 36, lootgroup : '', lootedid : ''};
+mappages["pitdespair1"].features[43] = {name : 'BottomChairWood', x : 15, y : 39};
+mappages["pitdespair1"].features[44] = {name : 'BookshelfOne', x : 12, y : 37};
+mappages["pitdespair1"].features[45] = {name : 'LeftTable', x : 14, y : 38};
+mappages["pitdespair1"].features[46] = {name : 'MiddleTable', x : 15, y : 38};
+mappages["pitdespair1"].features[47] = {name : 'RightTable', x : 16, y : 38};
 
 mappages["pitdespair1"].npcs = [];
+mappages["pitdespair1"].npcs[0] = {name : 'SlimeNPC', x : 8, y : 23};
+mappages["pitdespair1"].npcs[1] = {name : 'SlimeNPC', x : 9, y : 24};
+mappages["pitdespair1"].npcs[2] = {name : 'SlimeNPC', x : 8, y : 26};
+mappages["pitdespair1"].npcs[3] = {name : 'SlimeNPC', x : 10, y : 26};
+mappages["pitdespair1"].npcs[4] = {name : 'SlimeNPC', x : 21, y : 20};
+mappages["pitdespair1"].npcs[5] = {name : 'SlimeNPC', x : 22, y : 19};
+mappages["pitdespair1"].npcs[6] = {name : 'SlimeNPC', x : 22, y : 21};
+mappages["pitdespair1"].npcs[7] = {name : 'SlimeNPC', x : 23, y : 21};
+mappages["pitdespair1"].npcs[8] = {name : 'SlimeNPC', x : 24, y : 19};
+mappages["pitdespair1"].npcs[9] = {name : 'GiantInsectsNPC', x : 24, y : 20};
+mappages["pitdespair1"].npcs[10] = {name : 'GiantInsectsNPC', x : 24, y : 21};
+mappages["pitdespair1"].npcs[11] = {name : 'GiantInsectsNPC', x : 17, y : 23};
+mappages["pitdespair1"].npcs[12] = {name : 'GiantInsectsNPC', x : 18, y : 24};
+mappages["pitdespair1"].npcs[13] = {name : 'GiantInsectsNPC', x : 16, y : 25};
+mappages["pitdespair1"].npcs[14] = {name : 'GiantRatNPC', x : 16, y : 28};
+mappages["pitdespair1"].npcs[15] = {name : 'GiantRatNPC', x : 17, y : 28};
+mappages["pitdespair1"].npcs[16] = {name : 'GiantRatNPC', x : 17, y : 29};
+mappages["pitdespair1"].npcs[17] = {name : 'GiantRatNPC', x : 18, y : 30};
+mappages["pitdespair1"].npcs[18] = {name : 'GiantRatNPC', x : 18, y : 27};
+mappages["pitdespair1"].npcs[19] = {name : 'HoodNPC', x : 19, y : 11};
+mappages["pitdespair1"].npcs[20] = {name : 'HoodNPC', x : 20, y : 11};
+mappages["pitdespair1"].npcs[21] = {name : 'HoodNPC', x : 21, y : 12};
+mappages["pitdespair1"].npcs[22] = {name : 'RogueNPC', x : 20, y : 13};
+mappages["pitdespair1"].npcs[23] = {name : 'RogueNPC', x : 19, y : 14};
+mappages["pitdespair1"].npcs[24] = {name : 'RogueNPC', x : 21, y : 14};
+mappages["pitdespair1"].npcs[25] = {name : 'GiantBatNPC', x : 30, y : 29};
+mappages["pitdespair1"].npcs[26] = {name : 'GiantBatNPC', x : 31, y : 28};
+mappages["pitdespair1"].npcs[27] = {name : 'GiantBatNPC', x : 32, y : 30};
+mappages["pitdespair1"].npcs[28] = {name : 'GiantBatNPC', x : 30, y : 31};
+mappages["pitdespair1"].npcs[29] = {name : 'GiantBatNPC', x : 31, y : 32};
+mappages["pitdespair1"].npcs[30] = {name : 'GiantBatNPC', x : 20, y : 37};
+mappages["pitdespair1"].npcs[31] = {name : 'GiantBatNPC', x : 19, y : 38};
+mappages["pitdespair1"].npcs[32] = {name : 'GiantBatNPC', x : 20, y : 39};
+mappages["pitdespair1"].npcs[33] = {name : 'GiantRatNPC', x : 22, y : 37};
+mappages["pitdespair1"].npcs[34] = {name : 'GiantRatNPC', x : 23, y : 38};
+mappages["pitdespair1"].npcs[35] = {name : 'GiantRatNPC', x : 22, y : 39};
 
 mappages["pitdespair1"].desc = "Pit of Despair 1";
 mappages["pitdespair1"].music = 'Dungeon';
@@ -114,3 +157,125 @@ mappages["pitdespair1"].returnx = '117';
 mappages["pitdespair1"].returny = '76';
 mappages["pitdespair1"].returninfused = '0';
 mappages["pitdespair1"].linkedMaps = [""];
+
+
+// the following two functions add a quest key to two of the doors,
+// so they auto-unlock if you are holding it
+mappages["pitdespair1"].onload = function(mapref) {
+
+  var doortile = mapref.getTile(11,20);
+  var door = doortile.getTopFeature();
+  PDChangeUse(door);
+    
+  doortile = mapref.getTile(15,20);
+  door = doortile.getTopFeature();
+  PDChangeUse(door);
+
+  doortile = mapref.getTile(14,36);
+  door = doortile.getTopFeature();
+  PDChangeUse(door);
+    
+  if (gamestate.getMode() !== "loadgame") {
+    var ports = [];
+    doortile = mapref.getTile(11,24);
+    ports[0] = doortile.getTopFeature();
+    doortile = mapref.getTile(15,24);
+    ports[1] = doortile.getTopFeature();
+    doortile = mapref.getTile(15,28);
+    ports[2] = doortile.getTopFeature();
+    doortile = mapref.getTile(21,36);
+    ports[3] = doortile.getTopFeature();
+    doortile = mapref.getTile(29,29);
+    ports[4] = doortile.getTopFeature();
+    doortile = mapref.getTile(25,20);
+    ports[5] = doortile.getTopFeature();
+
+    $.each(ports, function(idx,val) {
+      val.setGraphicArray(["055.gif", "wall-arch.gif", 0, 0]);
+			
+			val.closedLOS = val.getBlocksLOSArray();
+			var seethru = [];
+			seethru[0] = 0;
+			val.setBlocksLOSArray(seethru);
+			
+			val.addPassable(MOVE_WALK);
+      val.open = 1;
+    });
+    
+    doortile = mapref.getTile(18,11);
+    var lever = doortile.getTopFeature();
+    lever.attached = { x: 11, y:9 };
+    doortile = mapref.getTile(18,12);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 11, y:14 };
+    doortile = mapref.getTile(18,13);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 11, y:24 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(18,14);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 15, y:24 };
+    doortile = mapref.getTile(18,15);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 11, y:31 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(18,16);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 15, y:28 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(18,17);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 21, y:36 };
+    doortile = mapref.getTile(20,17);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 27, y:36 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(22,17);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 25, y:29 };
+    doortile = mapref.getTile(22,16);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 29, y:29 };
+    doortile = mapref.getTile(22,15);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 25, y:24 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(22,14);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 29, y:24 };
+    doortile = mapref.getTile(22,13);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 25, y:20 };
+    doortile = mapref.getTile(22,12);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 29, y:20 };
+    lever.graphic = "switch-on.gif";
+    lever.overlay = "switch-on.gif";
+    doortile = mapref.getTile(22,11);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 29, y:14 };
+    doortile = mapref.getTile(22,10);
+    lever = doortile.getTopFeature();
+    lever.attached = { x: 29, y:9 };
+    
+  }
+}
+
+function PDChangeUse(obj) {
+  obj.realuse = obj.use;
+  obj.use = function(user) {
+    var retval = {};
+
+    if (user.inventory.getByName("PitOfDespairKey")) {
+      obj.unlockMe();
+    }
+    
+    retval = obj.realuse(user);
+    return retval;
+  }
+}
