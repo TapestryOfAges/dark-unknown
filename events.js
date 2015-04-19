00405ce742765abfd3a96fb278670112cd0a483e
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2015-04-19 21:54:42 +0000

    DUevents

--- events.js
+++ events.js
@@ -1,3 +1,5 @@
+// Finished but untested
+
 
 function DUListener() {
   this.listeners = new Collection();
@@ -8,7 +10,29 @@ DUListener.prototype.addListener = function(newlistener) {
   this.listeners.addTop(newlistener);
 }
 
+DUListener.prototype.clearListeners = function() {
+  var allears = this.listeners.getAll();
+  $.each(allears, function(idx, val) {
+    if (val.linkedtomap) {
+      var mapexists = maps.getMap(val.linkedtomap);
+      if (mapexists === undefined) {
+        this.clearListener(val.name);
+      }
+    }
+  });
+}
+
+DUListener.prototype.clearListener = function(listname) {
+  findlistener = this.listeners.getByName(listname);
+  if (findlistener) {
+    if (debug) { dbs.writeln("<span style='color:magenta'>Deleting listener " + findlistener.getName() + ".<br /></span>"); }
+    this.listeners.deleteFrom(findlistener);
+  }
+}
+
 DUListener.prototype.sendEvent = function(ev) {
+  this.clearListeners();
+  
   var allears = this.listeners.getAll();
   if (allears.length === 0) { 
     if (debug) { dbs.writeln("<span style='color:magenta'>Event sent, no listeners.<br /></span>"); }
@@ -24,16 +48,25 @@ DUListener.prototype.sendEvent = function(ev) {
           flagsmatch = 0;
         }
       });
+      if (flagsmatch) {
+        this.callfunc(ev);
+      }
     }
   });
 }
 
 function DUEar() {
+  this.name;
   this.listenforname;
   this.callfunc;
   this.flagsreq = {};
+  this.linkedtomap;  // name
 }
 DUEar.prototype = new Object();
+
+DUEar.prototype.getName = function() {
+  return this.name;
+}
 
 function DUEvent() {
   this.name;
