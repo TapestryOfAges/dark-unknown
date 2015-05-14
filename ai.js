edce35c9166a2ea157411345ae26ea2d2f20fde0
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2015-05-14 01:10:59 +0000

    sentinel- check for player

--- ai.js
+++ ai.js
@@ -21,6 +21,7 @@ ais.Sentinel = function(who) {
   // then, see if path is blocked, if so, if there is a jump, jump to next step without moving
   // if neither of those things happened, take the next step
   
+  var mymap = who.getHomeMap();
   var diffx=0;
   var diffy=0;
   if (destinations[who.patrol][who.step] === "n") {
@@ -34,7 +35,12 @@ ais.Sentinel = function(who) {
   } else {
     alert("Sentinels have an invalid step, " + who.patrol + " / " + who.step);
   }
-  
+
+  if ((PC.getHomeMap() === mymap) && (PC.getx() === who.getx()+diffx) && (PC.gety() === who.gety()+diffy)) {
+    if (debug) { dbs.writeln("<span style='color:orange;'>SENTINEL " + who.getserial() + ": PC in the way. Removing.</span><br />"); }
+    mymap.moveThing(16,13,PC);
+    maintext.addText("The sentinel teleports you away.");
+  }
 }
 
 ais.Bandit = function(who,radius) {
