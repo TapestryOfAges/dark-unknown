5de6b5c7c4f630f5005ea6655bd9ca92f1458a7c
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2015-05-22 08:32:05 +0000

    minor sound improvement

--- util.js
+++ util.js
@@ -238,7 +238,7 @@ function MoveBetweenMaps(who,frommap,tomap,destx,desty,overridetests) {
   }
 	
 	if ((DU.gameflags.music) && (who === PC) && (tomap.getMusic() !== nowplaying)) {
-	  StopMusic();
+	  StopMusic(nowplaying);
 	  var song = tomap.getMusic();
 	  PlaySound(song);
 	  nowplaying = song;
