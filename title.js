4bf1c9c35575e38fe86a45d3e2898143876742bd
madmanatw <madmanatw@a487bf6a-e058-45b0-8c56-482444db3515>
2015-06-29 08:22:46 +0000

    title screen/char create working

--- title.js
+++ title.js
@@ -27,18 +27,48 @@ optnames[1] = "graphics/title/create";
 optnames[2] = "graphics/title/journey";
 optnames[3] = "graphics/title/credits";
 
+var avatars = [];
+avatars[0] = ["300.2.gif", "300.gif", "301.gif", "shepherd-offcolor.gif", "302.gif", "druid-offcolor.gif"];
+avatars[1] = ["303.2.gif", "303.gif", "304.2.gif", "304.gif", "305.gif", "ranger-offcolor.gif"];
+avatars[2] = ["306.gif", "307.2.gif", "307.gif", "308.gif", "311.gif", "tinker-offcolor.gif"];
+avatars[3] = ["bard-offcolor.gif", "fighter-offcolor.gif", "paladin-offcolor.gif", "mage-offcolor.gif", "", ""];
+
+var avatarselect = {};
+avatarselect.x = 0;
+avatarselect.y = 0;
+
+var maxserial = 0;
 var mappages = new Pages();
 var localFactory = new tileFactory();
 var eidos = new Platonic();
 
+var gamestate = new GameStateData();
+gamestate.setMode("null");
 var DU = {};
 DU.gameflags = {};
 DU.gameflags.music = 1;
 DU.gameflags.sound = 1;
+DU.gameflags.tablet = 0;
+DU.gameflags.autosave = 0;
+DU.gameflags.negate = {};
 var nowplaying;
-var gamestate = "null";
 var optselect = 0;
 var charname = "";
+var gender = "";
+var graphic = "";
+var dusong;
+DU.DUTime = new Timeline(0);
+var DUTime = DU.DUTime; // alias
+DU.maps = new MapMemory();
+var maps = DU.maps; // alias
+var debug = 0;
+var PC = new PCObject();
+PC.assignSerial();
+
+var firsttime = 1;
+var themap;
+themap = new GameMap();
+
 
 $(document).ready(function() {
   audio_init();  
@@ -62,6 +92,7 @@ $(document).ready(function() {
 
   $("#maindiv").html(firstpage);
   $("#ToA").fadeIn(2000, function() {
+//    dusong = DUPlaySound("Dark Unknown");
     $("#and").fadeIn(1000, function() {
       $("#gf").fadeIn(400, function() {
         $("#present").fadeIn(1400, function() {
@@ -80,7 +111,6 @@ $(document).ready(function() {
 });
 
 function SecondPage() {
-//  var dusong = DUPlaySound("Dark Unknown");
   
   var browserheight = $(window).height();
   var browserwidth = $(window).width();
@@ -89,6 +119,7 @@ function SecondPage() {
   var sptop = browserheight/2 - 300;
   var opttop = sptop + 250;
   var optleft = browserwidth/2 - 215;
+  optselect = 0;
   var spage = "<div id='DU' style='position:absolute;left:" + sleft + "px;top:" + sptop + "px;display:none'><img src='graphics/title/du_logo.gif' /></div><div id='options'></div>";
   $("#maindiv").html(spage);
   $("#DU").fadeIn(1000, function() {
@@ -101,6 +132,8 @@ function SecondPage() {
     if (!localStorage.savegame) {
       journey = "journey-d.gif";
       optnames[2] = "graphics/title/journey-d";
+    } else {
+      optnames[2] = "graphics/title/journey";
     }
     spage += "<div id='journey' style='position:absolute;left:" + optleft + "px; top:" + opttop + "px;display:none'><img id='opt2' src='graphics/title/" + journey + "' onClick='makeChoice(\'journey\')' /></div>";
     opttop += 60;
@@ -114,21 +147,24 @@ function SecondPage() {
 }
 
 function pagelive() {
-  gamestate = "on";
+  gamestate.setMode("on");
   
-  $(document).keydown(function(e) {
-    var code = (e.keyCode ? e.keyCode : e.which);
-    e.preventDefault();
-    if (gamestate === "on") {
-      DoAction(code);
-    }
-  });
-
+  if (firsttime) {
+    $(document).keydown(function(e) {
+      var code = (e.keyCode ? e.keyCode : e.which);
+      e.preventDefault();
+    
+      if (gamestate.getMode() !== "null") {
+        DoAction(code, e);
+      }
+    });
+    firsttime = 0;
+  }
 }
 
 
-function DoAction(code) {
-  if (gamestate === "on") {
+function DoAction(code, e) {
+  if (gamestate.getMode() === "on") {
     if ((code === 38) || (code === 219)) {
       if (optselect > 0) {
         var img = "opt" + optselect;
@@ -167,17 +203,20 @@ function DoAction(code) {
         CharCreate();
       }
       else if (optselect === 2) {
-        // load game.html
+        window.open("game.html", "_self");
       }
       else if (optselect === 3) {
         alert("credits");
       }
     }
   }
-  else if (gamestate === "name") {
+  else if (gamestate.getMode() === "name") {
     if (((code >= 65) && (code <= 90)) || (code === 32)) {
       if (charname.length < 15) {
         var letter = String.fromCharCode(code);  
+        if (!e.shiftKey) {
+          letter = letter.toLowerCase();
+        }
         charname += letter;
         var chartxt = "Enter character name: <span style='color:gold'>" + charname + "</span>";
         if (charname.length < 15) { chartxt += "_"; }
@@ -189,6 +228,53 @@ function DoAction(code) {
         var chartxt = "Enter character name: <span style='color:gold'>" + charname + "</span>_";
         $("#charprompt").html(chartxt);
       }
+    } else if (code === 13) { // enter
+      if (charname.length) {
+        var chartxt = "<span style='color:gold'>" + charname + "</span><br /><br />Specify your gender: (M)ale, (F)emale, or (O)ther/decline to state";
+        $("#charprompt").html(chartxt);
+        gamestate.setMode("gender");
+      }
+    }
+  } else if (gamestate.getMode() === "gender") {
+    if (code === 70) {
+      gender = "female";
+      ChooseGraphic();
+    } else if (code === 79) {
+      gender = "neuter";
+      ChooseGraphic();
+    } else if (code === 77) {
+      gender = "male";
+      ChooseGraphic();
+    }
+  } else if (gamestate.getMode() === "graphic") {
+    if ((code === 38) || (code === 219)) { // up
+      if (avatarselect.y > 0) {
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","black");
+        avatarselect.y--;
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","white");
+      }
+    } else if ((code === 37) || (code === 59)) {  // left
+      if (avatarselect.x > 0) {
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","black");
+        avatarselect.x--;
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","white");
+      }
+    } else if ((code === 39) || (code === 222)) { // right
+      if (((avatarselect.y === 3) && (avatarselect.x < 3)) || ((avatarselect.y < 3) && (avatarselect.x < 5))) {
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","black");
+        avatarselect.x++;
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","white");
+      }
+    } else if ((code === 40) || (code === 191)) {
+      if (((avatarselect.x > 3) && (avatarselect.y < 2)) || ((avatarselect.y < 3) && (avatarselect.x < 4))) {
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","black");
+        avatarselect.y++;
+        $("#" + avatarselect.y + "x" + avatarselect.x).css("background-color","white");
+      }
+    } else if ((code === 32) || (code === 13)) { // space or enter
+      graphic = avatars[avatarselect.y][avatarselect.x];
+      SaveChar();
+      SecondPage();
     }
   }
 }
@@ -196,5 +282,56 @@ function DoAction(code) {
 function CharCreate() {
   var charprompt = "<div style='position:absolute;left:100px;top:100px'><p class='charcreate' id='charprompt'>Enter character name: _</p></div>";
   $("#maindiv").html(charprompt);
-  gamestate = "name";
+  gamestate.setMode("name");
+}
+
+function ChooseGraphic() {
+  gamestate.setMode("graphic");
+  var chartxt = "<div style='position:absolute;left:100px;top:100px'><p class='charcreate'><span style='color:gold'>" + charname + "</span><br /><span style='color:white'>";
+  if (gender === "male") { chartxt += "Male"; }
+  if (gender === "female") { chartxt += "Female"; }
+  if (gender === "neuter") { chartxt += "Other"; }
+  chartxt += "</span></p><p class='charcreate'>Choose your avatar:</p>";
+  
+  chartxt += "<table cellpadding='2' cellspacing='10' cellborder='0'>";
+  for (var i=0; i<=3; i++) {
+    chartxt += "<tr>";
+    for (var j=0; j<=5; j++) {
+      if ((i!==3) || (j<4)) { 
+        chartxt += "<td id='" + i + "x" + j + "'><img src='graphics/" + avatars[i][j] + "' /></td>";
+      }
+    }
+    chartxt += "</tr>";
+  }
+  chartxt += "</table></div>";
+  $("#maindiv").html(chartxt);
+  
+  $("#0x0").css("background-color","white");
+}
+
+function SaveChar() {
+  PC.setPCName(charname);
+  PC.setGraphic(graphic);
+  PC.setGender(gender);
+  
+  themap.loadMap("darkunknown");
+  maps.addMapByRef(themap);
+
+  PC.setHomeMap(themap);
+  PC.setx(69);
+  PC.sety(74);
+  PC.getHomeMap().placeThing(PC.getx(),PC.gety(),PC);
+  var dagger = localFactory.createTile("Dagger");
+  PC.addToInventory(dagger, 1);
+  PC.addGold(100);
+  PC.setEquipment("weapon",dagger);
+  var armor = localFactory.createTile("ClothArmor");
+  PC.addToInventory(armor, 1);
+  PC.setEquipment("armor",armor);
+
+var PCEvent = new GameEvent(PC);
+	DUTime.addAtTimeInterval(PCEvent,.0001);
+	startScheduler();
+	
+	gamestate.saveGame();
 }
