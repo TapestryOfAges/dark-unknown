6479b32e0693f69513ce20326590c34abfbb0d42
madmanatw <gf@tapestryofages.com>
2015-06-30 08:13:08 +0000

    added LoE check

--- ephemeralObjects.js
+++ ephemeralObjects.js
@@ -400,13 +400,16 @@ MirrorWardTile.prototype.findNewTarget = function(caster) {
   var displayspecs = getDisplayCenter(curtarget.getHomeMap(),caster.getx(),caster.gety());
   var pcdisplay = getDisplayCenter(curtarget.getHomeMap(),PC.getx(),PC.gety()); // just in case
   
+  if (debug) { dbs.writeln("<span style='color:green'>MIRROR WARD is reflecting the spell...<br /></span>"); }
   $.each(localnpcs, function(idx, val) {
     if (val !== curtarget) {
       if (GetDistance(val.getx(), val.gety(), tgt.getx(), tgt.gety()) < 8.5) {
         // check for on screen from caster
         if ((displayspecs.leftedge <= val.getx()) && (val.getx() <= displayspecs.rightedge) && (displayspecs.topedge <= val.gety()) && (val.gety() <= displayspecs.bottomedge)) {
-          // check for LoE from curtarget WORKING HERE
-          newtgt.push(val);
+          // check for LoE from curtarget 
+          if (curtarget.getHomeMap().getLOS(curtarget.getx(), curtarget.gety(), val.getx(), val.gety(), losgrid, 1) > LOS_THRESHOLD) { 
+            newtgt.push(val);
+          }
         }
       }
     }
