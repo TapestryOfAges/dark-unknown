fb938ac2808a2c89380a3eddf55dd200a23b1bcd
madmanatw <gf@tapestryofages.com>
2016-02-15 09:50:41 +0000

    unit tests

--- test/DUtests.js
+++ test/DUtests.js
@@ -1,67 +1,169 @@
 
-// Dice test
-QUnit.test( "Test dice parse", function( assert ) {
-  var Dice = new DiceObject();
-  var dieobj = Dice.parse("3d6+2");
-  assert.deepEqual( dieobj.quantity, 3, "Checking '3d6+2', expecting 3" );
-  assert.deepEqual( dieobj.dice, 6, "Checking '3d6+2', expecting 6" );
-  assert.deepEqual( dieobj.plus, 2, "Checking '3d6+2', expecting 2" );
+// list of necessary tests: map load?
+
+var maxserial = 0;
+var viewsizex = 13;
+var viewsizey = 13;
+var DAYNIGHT = DAY;
+
+var wind = {};
+wind.xoff = 0;
+wind.yoff = 2;
+
+var mappages = new Pages();
+var localFactory = new tileFactory();
+var eidos = new Platonic();
+
+var DU = {};  // attach all saveable global objects to me
+
+DU.PC = new PCObject();
+var PC = DU.PC;  // alias
+
+var timeouts = {};
+
+PC.assignSerial();
+var nowplaying;
+var laststep = "left";
+DU.maps = new MapMemory();
+var maps = DU.maps; // alias
+var losgrid = new LOSMatrix(30);  // BIGGER FOR AI USE
+
+DU.DUTime = new Timeline(0);
+var DUTime = DU.DUTime; // alias
+var maintext = new TextFrame("innertextframe");
+var DULoot = SetLoots();            //
+var DULootGroups = SetLootGroups(); //  see loot.js and lootset.js for population
+var DUTraps = SetTraps();           //
+var displayspecs = {};
+var Dice = new DiceObject();
+var finder = new PF.AStarFinder({
+  heuristic: PF.Heuristic.euclidean
+});
+DU.gameflags = new Gameflags();
+
+var targetCursor = {};
+    targetCursor.skipahead = 0;
+var inputText = {};
+
+var raceWarning = 0;
+
+
+function DrawCharFrame() {}
+function ShowEffect(onwhat, duration, graphic, xoff, yoff) {}
   
-  dieobj = Dice.parse("3d6+-2");
-  assert.deepEqual( dieobj.quantity, 3, "Checking '3d6+-2', expecting 3" );
-  assert.deepEqual( dieobj.dice, 6, "Checking '3d6+-2', expecting 6" );
-  assert.deepEqual( dieobj.plus, -2, "Checking '3d6+-2', expecting -2" );
+//maps.addMap("unittest");
 
-  dieobj = Dice.parse("3d6-2");
-  assert.deepEqual( dieobj.quantity, 3, "Checking '3d6-2', expecting 3" );
-  assert.deepEqual( dieobj.dice, 6, "Checking '3d6-2', expecting 6" );
-  assert.deepEqual( dieobj.plus, -2, "Checking '3d6-2', expecting -2" );
+//function ClearMap(mapref) {
+//  var fea = mapref.features.getAll();
+//  var npcs = mapref.npcs.getAll();
+//  $.each(fea, function(idx, val) {
+//    mapref.deleteThing(val);
+//  });
+//  $.each(npcs, function(idx,val) {
+//    mapref.deleteThing(val);
+//  });
+//}  
 
-  dieobj = Dice.parse("2d10");
-  assert.deepEqual( dieobj.quantity, 2, "Checking '2d10', expecting 2" );
-  assert.deepEqual( dieobj.dice, 10, "Checking '2d10', expecting 10" );
-  assert.deepEqual( dieobj.plus, 0, "Checking '2d10', expecting 0" );
+QUnit.test( "Test Awaken spell", function( assert ) {
+  maps.addMap("unittest");
+  var testmap = maps.getMap("unittest");
 
-  dieobj = Dice.parse("4");
-  assert.deepEqual( dieobj.quantity, 0, "Checking '4', expecting 0" );
-  assert.deepEqual( dieobj.dice, 0, "Checking '4', expecting 0" );
-  assert.deepEqual( dieobj.plus, 4, "Checking '4', expecting 4" );
+  var tgtmob = localFactory.createTile("TownGuardNPC");
+  var castermob = localFactory.createTile("PaladinNPC");
+  
+  testmap.placeThing(5,4,tgtmob);
+  testmap.placeThing(5,8,castermob);
+  
+  var awake = PerformAwaken(castermob,0,0,tgtmob);
+  assert.deepEqual(awake["txt"], "Your spell cannot reach that target!", "Spell can't reach.");
 
-  dieobj = Dice.parse(4);
-  assert.deepEqual( dieobj.quantity, 0, "Checking '4', expecting 0" );
-  assert.deepEqual( dieobj.dice, 0, "Checking '4', expecting 0" );
-  assert.deepEqual( dieobj.plus, 4, "Checking '4', expecting 4" );
-    
-  dieobj = Dice.parse("+4");
-  assert.deepEqual( dieobj.quantity, 0, "Checking '+4', expecting 0" );
-  assert.deepEqual( dieobj.dice, 0, "Checking '+4', expecting 0" );
-  assert.deepEqual( dieobj.plus, 4, "Checking '+4', expecting 4" );
+  testmap.moveThing(9,8,tgtmob);
+  var oldmana = castermob.getMana();
+  
+  awake = PerformAwaken(castermob,0,0,tgtmob);
+  assert.deepEqual(oldmana - castermob.getMana(), 1, "Awaken cast, should cost 1.");
+  assert.deepEqual(awake["txt"], "Guard is not asleep- no effect.", "Checking response on non-sleeping target.");
+  
+  var sleeptile = localFactory.createTile("Sleep");
+  tgtmob.addSpellEffect(sleeptile);
+  var asleep = tgtmob.getSpellEffectsByName("Sleep");
+  var isasleep = 0;
+  if (asleep) { isasleep = 1; }
+  oldmana = castermob.getMana();
+  assert.deepEqual(isasleep, 1, "Putting guard to sleep before spellcast.");
+  awake = PerformAwaken(castermob,0,1,tgtmob);
+  assert.deepEqual(oldmana - castermob.getMana(), 0, "Awaken cast, should cost 0 (free cast).");
+  asleep = tgtmob.getSpellEffectsByName("Sleep");
+  isasleep = 0;
+  if (asleep) { isasleep = 1; }
+  assert.deepEqual(isasleep, 0, "Guard is now awake.");
 
-  dieobj = Dice.parse("-4");
-  assert.deepEqual( dieobj.quantity, 0, "Checking '-4', expecting 0" );
-  assert.deepEqual( dieobj.dice, 0, "Checking '-4', expecting 0" );
-  assert.deepEqual( dieobj.plus, -4, "Checking '-4', expecting -4" );
-
-  dieobj = Dice.parse(-4);
-  assert.deepEqual( dieobj.quantity, 0, "Checking '-4', expecting 0" );
-  assert.deepEqual( dieobj.dice, 0, "Checking '-4', expecting 0" );
-  assert.deepEqual( dieobj.plus, -4, "Checking '-4', expecting -4" );
-
-  dieobj = Dice.parse("3d");
-  assert.deepEqual( dieobj.quantity, 0, "Checking '3d', expecting 0" );
-  assert.deepEqual( dieobj.dice, 1, "Checking '3d', expecting 1" );
-  assert.deepEqual( dieobj.plus, 0, "Checking '3d', expecting 0" );
-
-  dieobj = Dice.parse("3d+2");
-  assert.deepEqual( dieobj.quantity, 0, "Checking '3d+2', expecting 0" );
-  assert.deepEqual( dieobj.dice, 1, "Checking '3d+2', expecting 1" );
-  assert.deepEqual( dieobj.plus, 2, "Checking '3d+2', expecting 2" );
-
-  dieobj = Dice.parse("d2+3");
-  assert.deepEqual( dieobj.quantity, 1, "Checking 'd2+3', expecting 1" );
-  assert.deepEqual( dieobj.dice, 2, "Checking 'd2+3', expecting 2" );
-  assert.deepEqual( dieobj.plus, 3, "Checking 'd2+3', expecting 3" );
-
+  oldmana = castermob.getMana();
+  awake = PerformAwaken(castermob,1,0,tgtmob);
+  assert.deepEqual(oldmana - castermob.getMana(), 2, "Cast infused, should cost 2.");
+  
+  maps.deleteMap("unittest");
 });
 
+QUnit.test( "Test Cure spell", function( assert ) {
+  maps.addMap("unittest");
+  var testmap = maps.getMap("unittest");
+  
+  var castermob = localFactory.createTile("PaladinNPC");
+  testmap.placeThing(5,8,castermob);
 
+  var poison = localFactory.createTile("Poison");
+  var disease = localFactory.createTile("Disease");
+  castermob.addSpellEffect(poison);
+  castermob.addSpellEffect(disease);
+  
+  var ispoisoned = 0;
+  var isdiseased = 0;
+  if (castermob.getSpellEffectsByName("Poison")) { ispoisoned = 1; }
+  if (castermob.getSpellEffectsByName("Disease")) { isdiseased = 1; }
+  assert.deepEqual(ispoisoned, 1, "Caster starts poisoned.");
+  assert.deepEqual(isdiseased, 1, "Caster starts diseased.");
+  
+  var oldmana = castermob.getMana();
+  var cure = magic[1][GetSpellID(2)].executeSpell(castermob,0,0);
+  ispoisoned = 0;
+  isdiseased = 0;
+  if (castermob.getSpellEffectsByName("poison")) { ispoisoned = 1; }
+  if (castermob.getSpellEffectsByName("disease")) { isdiseased = 1; }
+  
+  assert.deepEqual(oldmana-castermob.getMana(), 1, "Cost 1 mana.");
+  assert.deepEqual(ispoisoned, 0, "Caster is cured of poison.");
+  assert.deepEqual(isdiseased, 0, "Caster is cured of disease.");
+
+  var poison = localFactory.createTile("Poison");
+  var disease = localFactory.createTile("Disease");
+  castermob.addSpellEffect(poison);
+  castermob.addSpellEffect(disease);
+  
+  var ispoisoned = 0;
+  var isdiseased = 0;
+  if (castermob.getSpellEffectsByName("Poison")) { ispoisoned = 1; }
+  if (castermob.getSpellEffectsByName("Disease")) { isdiseased = 1; }
+  
+  Dice.roll = function(die) { return 4; }
+  castermob.healMe = function(amt) {
+    castermob.washealed = amt;
+  }
+  var oldmana = castermob.getMana();
+  var cure = magic[1][GetSpellID(2)].executeSpell(castermob,1,0);
+  ispoisoned = 0;
+  isdiseased = 0;
+  if (castermob.getSpellEffectsByName("poison")) { ispoisoned = 1; }
+  if (castermob.getSpellEffectsByName("disease")) { isdiseased = 1; }
+  
+  assert.deepEqual(oldmana-castermob.getMana(), 2, "Infused: cost 2 mana.");
+  assert.deepEqual(ispoisoned, 0, "Caster is cured of poison.");
+  assert.deepEqual(isdiseased, 0, "Caster is cured of disease.");
+  assert.deepEqual(castermob.washealed, 4, "Caster was healed for 4.");
+
+  maps.deleteMap("unittest");
+});
+
+QUnit.test( "Test Disarm Trap spell", function( assert ) {
+  
+});
