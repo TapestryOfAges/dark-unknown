95b45cf2c7e504d687c5b72d8b0af33e721d929a
madmanatw <gf@tapestryofages.com>
2016-08-18 08:34:12 +0000

    added the rest of the audachta, moved unused, fixed some graphics

--- gameObjectsUnused.js
+++ gameObjectsUnused.js
@@ -165,3 +165,423 @@ function RiverSourceWTile() {
   this.desc = "a river";
 }
 RiverSourceWTile.prototype = new TerrainObject;
+
+/*
+function FountainSWTile() {
+  this.name = "FountainSW";
+//  this.graphic = "017.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-32";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_FLY + MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.prefix = "a";
+  this.desc = "fountain";
+  this.peerview = "white";
+}
+FountainSWTile.prototype = new TerrainObject();
+
+function FountainSETile() {
+  this.name = "FountainSE";
+//  this.graphic = "018.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "0";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_FLY + MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.prefix = "a";
+  this.desc = "fountain";
+  this.peerview = "white";
+}
+FountainSETile.prototype = new TerrainObject();
+
+function FountainNWTile() {
+  this.name = "FountainNW";
+//  this.graphic = "019.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-32";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_FLY + MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.prefix = "a";
+  this.desc = "fountain";
+}
+FountainNWTile.prototype = new TerrainObject();
+
+function FountainNETile() {
+  this.name = "FountainNE";
+//  this.graphic = "020.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "0";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_FLY + MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.prefix = "a";
+  this.desc = "fountain";
+}
+FountainNETile.prototype = new TerrainObject();
+
+function LetterATile() {
+  this.name = "LetterA";
+//  this.graphic = "025.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-64";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "A";
+}
+LetterATile.prototype = new TerrainObject();
+
+function LetterBTile() {
+  this.name = "LetterB";
+//  this.graphic = "026.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-96";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "B";
+}
+LetterBTile.prototype = new TerrainObject();
+
+function LetterCTile() {
+  this.name = "LetterC";
+//  this.graphic = "027.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-128";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "C";
+}
+LetterCTile.prototype = new TerrainObject();
+
+function LetterDTile() {
+  this.name = "LetterD";
+//  this.graphic = "028.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-160";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "D";
+}
+LetterDTile.prototype = new TerrainObject();
+
+function LetterETile() {
+  this.name = "LetterE";
+//  this.graphic = "029.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-192";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "E";
+}
+LetterETile.prototype = new TerrainObject();
+
+function LetterFTile() {
+  this.name = "LetterF";
+//  this.graphic = "030.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-224";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "F";
+}
+LetterFTile.prototype = new TerrainObject();
+
+function LetterGTile() {
+  this.name = "LetterG";
+//  this.graphic = "031.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-256";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "G";
+}
+LetterGTile.prototype = new TerrainObject();
+
+function LetterHTile() {
+  this.name = "LetterH";
+//  this.graphic = "032.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-288";
+  this.spriteyoffset = "-32";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "H";
+}
+LetterHTile.prototype = new TerrainObject();
+
+function LetterITile() {
+  this.name = "LetterI";
+//  this.graphic = "033.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-64";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "I";
+}
+LetterITile.prototype = new TerrainObject();
+
+function LetterJTile() {
+  this.name = "LetterJ";
+//  this.graphic = "034.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-96";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "J";
+}
+LetterJTile.prototype = new TerrainObject();
+
+function LetterKTile() {
+  this.name = "LetterK";
+//  this.graphic = "035.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-128";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "K";
+}
+LetterKTile.prototype = new TerrainObject();
+
+function LetterLTile() {
+  this.name = "LetterL";
+//  this.graphic = "036.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-160";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "L";
+}
+LetterLTile.prototype = new TerrainObject();
+
+function LetterMTile() {
+  this.name = "LetterM";
+//  this.graphic = "037.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-192";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "M";
+}
+LetterMTile.prototype = new TerrainObject();
+
+function LetterNTile() {
+  this.name = "LetterN";
+//  this.graphic = "038.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-224";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "N";
+}
+LetterNTile.prototype = new TerrainObject();
+
+function LetterOTile() {
+  this.name = "LetterO";
+//  this.graphic = "039.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-256";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "O";
+}
+LetterOTile.prototype = new TerrainObject();
+
+function LetterPTile() {
+  this.name = "LetterP";
+//  this.graphic = "040.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-288";
+  this.spriteyoffset = "-64";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "P";
+}
+LetterPTile.prototype = new TerrainObject();
+
+function LetterQTile() {
+  this.name = "LetterQ";
+//  this.graphic = "041.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "0";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "Q";
+}
+LetterQTile.prototype = new TerrainObject();
+
+function LetterRTile() {
+  this.name = "LetterR";
+//  this.graphic = "042.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-32";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "R";
+}
+LetterRTile.prototype = new TerrainObject();
+
+function LetterSTile() {
+  this.name = "LetterS";
+//  this.graphic = "043.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-64";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "S";
+}
+LetterSTile.prototype = new TerrainObject();
+
+function LetterTTile() {
+  this.name = "LetterT";
+//  this.graphic = "044.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-96";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "T";
+}
+LetterTTile.prototype = new TerrainObject();
+
+function LetterUTile() {
+  this.name = "LetterU";
+//  this.graphic = "045.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-128";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "U";
+}
+LetterUTile.prototype = new TerrainObject();
+
+function LetterVTile() {
+  this.name = "LetterV";
+//  this.graphic = "046.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-160";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "V";
+}
+LetterVTile.prototype = new TerrainObject();
+
+function LetterWTile() {
+  this.name = "LetterW";
+//  this.graphic = "047.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-192";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "W";
+}
+LetterWTile.prototype = new TerrainObject();
+
+function LetterXTile() {
+  this.name = "LetterX";
+//  this.graphic = "048.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-224";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "an";
+  this.desc = "X";
+}
+LetterXTile.prototype = new TerrainObject();
+
+function LetterYTile() {
+  this.name = "LetterY";
+//  this.graphic = "049.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-256";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "Y";
+}
+LetterYTile.prototype = new TerrainObject();
+
+function LetterZTile() {
+  this.name = "LetterZ";
+//  this.graphic = "050.gif";
+  this.graphic = "terrain_tiles.gif";
+  this.spritexoffset = "-288";
+  this.spriteyoffset = "-96";
+  this.passable = MOVE_ETHEREAL;
+  this.blocklos = 0;
+  this.blockloe = .3;
+  this.prefix = "a";
+  this.desc = "Z";
+}
+LetterZTile.prototype = new TerrainObject();
+*/
