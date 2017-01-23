d0d31fc7980a6f8ef2680620f8f006094eb00773
TapestryOfAges <gf@tapestryofages.com>
2017-01-22 20:48:17 -0800

    moving title into demo

--- title.js
+++ title.js
@@ -279,6 +279,7 @@ function pagelive() {
 function DoAction(code, e) {
   if (gamestate.getMode() === "intro") {
     RunIntro(introidx);
+    introidx++;
   }
   if (gamestate.getMode() === "on") {
     if ((code === 38) || (code === 219)) {    // up arrow or [
@@ -315,6 +316,7 @@ function DoAction(code, e) {
       if (optselect === 0) {
         introidx = 0;
         RunIntro(introidx);
+        introidx++;
       }
       else if (optselect === 1) {
         CharCreate();
@@ -501,7 +503,6 @@ function RunIntro(idx) {
       $('#maindiv').fadeIn(1000);
       gamestate.setMode("intro");
     });
-    introidx++;
   } else if (idx === 1) {
     var secondpage = "<div id='intro2' style='color:white;display:none'><p class='charcreate'>And Lance seemed made for the role. All things came easily to him- his studies of magic, of combat, of dance, of diplomacy. Which makes these events all the more surprising.</p></div>";
     $('#introcontainer').append(secondpage);
@@ -512,7 +513,7 @@ function RunIntro(idx) {
     $('#intro3').fadeIn(1000);
   } else if (idx === 3) {
     var nextpage = "<div id='intro4' style='color:white;display:none'><p class='charcreate'>The land has been at peace since the end of the civil war nearly 40 years ago. Six years ago, your grandfather passed away and your parents ascended the throne of Ellusus. The transition was smooth, and while King Erik was beloved as the one who had ended the war, that goodwill had seemed to pass readily enough to the new monarchs.</p></div>";
-    $('#introcontainer').append(nextpage);
+    $('#introcontainer').html(nextpage);
     $('#intro4').fadeIn(1000);
   } else if (idx === 4) {
     var nextpage = "<div id='intro5' style='color:white;display:none'><p class='charcreate'>Lance, then, completed his tutelage a few years ago, and was then charged with getting to know the kingdom, and so he has been away traveling, and you have not seen him in some time. There are rumors of the time he has spent- he has saved an old crone, and won a boon; he has battled a dragon; he has fallen into drunkeness and embarrassed your parents.</p></div>";
@@ -520,12 +521,16 @@ function RunIntro(idx) {
     $('#intro5').fadeIn(1000);
   } else if (idx === 5) {
     var nextpage = "<div id='intro6' style='color:white;display:none'><p class='charcreate'>But a few months ago, Lance moved into an old castle, ruined from the war, and begun rebuilding. And then, to the surprise of everyone, he planted his banner and declared that he was in rebellion- that he, rather than your father, should rule Ellusus.</p></div>";
-    $('#introcontainer').append(nextpage);
+    $('#introcontainer').html(nextpage);
     $('#intro6').fadeIn(1000);
   } else if (idx === 6) {
     var nextpage = "<div id='intro7' style='color:white;display:none'><p class='charcreate'>Shocked and saddened, your parents have summoned you to the ruling seat, Castle dea Olympus. The time for study is over. The time for leisure is past. As you stand now outside the gates, you prepare to enter and learn what lies in store for you...</p></div>";
     $('#introcontainer').append(nextpage);
     $('#intro7').fadeIn(1000);
+  } else if (idx === 7) {
+    gamestate.setMode("null");
+    SecondPage();
   }
+  return;
 }
 
