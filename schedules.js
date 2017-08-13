65ab71b46d4f955084ddd72927fae9d840e0f7b5
TapestryOfAges <gf@tapestryofages.com>
2017-08-13 02:28:16 -0700

    schedules

--- schedules.js
+++ schedules.js
@@ -3,14 +3,15 @@
 function NPCSchedule() {
   this.scheduleArray = [];
   this.currentIndex = 0;
+  this.baseLocation = "";
 }
 NPCSchedule.prototype = new Object();
 
-NPCSchedule.prototype.addActivity(activity) {
+NPCSchedule.prototype.addActivity = function(activity) {
   this.scheduleArray.push(activity);
 }
 
-NPCSchedule.prototype.getNPCLocationByTime(time) {
+NPCSchedule.prototype.getNPCLocationByTime = function(time) {
   if (!time) {
     time = GetClockTime();
   }
@@ -32,48 +33,49 @@ NPCSchedule.prototype.getNPCLocationByTime(time) {
 }
 
 function NPCActivity(type, params) {
+  this.params = {};
   if (type === "RouteTo") {  // Route To location
-    if (params.hasOwnProperty(destination)) {
-      if (params.destination.hasOwnProperty(x) && params.destination.hasOwnProperty(y)) {
-        this.destination = params.destination;
+    if (params.hasOwnProperty("destination")) {
+      if (params.destination.hasOwnProperty("x") && params.destination.hasOwnProperty("y")) {
+        this.params.destination = params.destination;
       } else { alert("Invalid destination."); }
     } else { alert("This activity is missing its destination."); }
-    if (params.hasOwnProperty(endCondition)) {
+    if (params.hasOwnProperty("endCondition")) {
       if (params.endCondition === "Time") {
-        if (params.hasOwnProperty(endTime)) {
-          this.endTime = params.endTime;
+        if (params.hasOwnProperty("endTime")) {
+          this.params.endTime = params.endTime;
         } else {
           alert("This activity's endCondition (Time) is missing a time."); 
         }
       }
     }
   } else if (type === "WaitHere") {  // Hang out in current location
-    if (params.hasOwnProperty("sleep")) { this.sleep = params.sleep; }
+    if (params.hasOwnProperty("sleep")) { this.params.sleep = params.sleep; }
     
-    if (params.hasOwnProperty("leashLength")) { this.leashLength = params.leashLength; }
+    if (params.hasOwnProperty("leashLength")) { this.params.leashLength = params.leashLength; }
     else { alert("This activity has no leashLength property!"); }    // should have property even if just 0
 
-    if (params.hasOwnProperty("responsibleFor")) { this.responsibleFor = params.responsibleFor; }  // array of coordinates for doors
+    if (params.hasOwnProperty("responsibleFor")) { this.params.responsibleFor = params.responsibleFor; }  // array of coordinates for doors
         
   } else if (type === "ChangeMap") {  // Can only move to a linked map
     if (params.hasOwnProperty("destination")) { 
       if (params.destination.hasOwnProperty("mapName") && params.destination.hasOwnProperty("x") && params.destination.hasOwnProperty("y")) {
-        this.destination = params.destination;
+        this.params.destination = params.destination;
       } else { alert("This activity's destination is incomplete!"); }
     } else { alert("This activity is missing the destination!"); }
 
   } else if (type === "LeaveMap") { // exit to world map
 
   } else if (type === "CallAI") { // Something other than what's here
-    if (params.hasOwnProperty("AIName")) { this.AIName = params.AIName; }
+    if (params.hasOwnProperty("AIName")) { this.params.AIName = params.AIName; }
     else { alert("This activity is missing its AIName!"); }
 
-    if (params.hasOwnProperty("params")) { this.params = params.params; } // nicely recursive, eh?
+    if (params.hasOwnProperty("params")) { this.params.params = params.params; } // nicely recursive, eh?
     else { alert("This activity is missing params for its AI!"); }
 
-    if (params.hasOwnProperty(endCondition)) {
+    if (params.hasOwnProperty("endCondition")) {
       if (params.endCondition === "Time") {
-        if (params.hasOwnProperty(endTime)) {
+        if (params.hasOwnProperty("endTime")) {
           this.endTime = params.endTime;
         } else {
           alert("This activity's endCondition (Time) is missing a time."); 
@@ -84,26 +86,31 @@ function NPCActivity(type, params) {
   }
 
   // All activities
-  if (params.hasOwnProperty(startCondition)) {
+  if (params.hasOwnProperty("startCondition")) {
     if (params.startCondition === "Time") {
-      this.startCondition = params.startCondition;
+      this.params.startCondition = params.startCondition;
       if (params.hasOwnProperty("startTime")) {
-        this.startTime = params.startTime;
+        this.params.startTime = params.startTime;
       } else { alert("This activity's startCondition (Time) is missing a time."); }
     } else if (params.startCondition === "PreviousComplete") {
-
+      this.params.startCondition = params.startCondition;
     } else { alert("This activity's startCondition is invalid."); }
   } else { alert("This activity is missing its start condition."); }
 
   if (params.hasOwnProperty("setFlag")) {
-    this.setFlag = params.setFlag;  // as usual, "unset_" works to unset
+    this.params.setFlag = params.setFlag;  // as usual, "unset_" works to unset
   }
 
   if (params.hasOwnProperty("bark") && params.hasOwnProperty("barkfreq")) {
-    this.bark = params.bark;  // array of bark txts
-    this.barkfreq = params.barkfreq;
+    this.params.bark = params.bark;  // array of bark txts
+    this.params.barkfreq = params.barkfreq;
   } else if (params.hasOwnProperty("bark") || params.hasOwnProperty("barkfreq")) {
     alert("Activity has incomplete bark information.");
   }
 }
 NPCActivity.prototype = new Object();
+
+function set_schedules() {
+  DU.schedules = {};
+
+}
