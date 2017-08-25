58cdbacb4084b8098cdbe2c6d5d01b8de6cf9b27
TapestryOfAges <gf@tapestryofages.com>
2017-08-24 17:11:46 -0700

    added many schedule-ai routines

--- ai_schedules.js
+++ ai_schedules.js
@@ -1,20 +1,275 @@
 "use strict";
 
-function ScheduledAI(who) {
-  var nextidx = who.getCurrentScheduleIndex + 1;
+// universal parameters:
+// Bark information (bark, barkfreq, barkrad)
+// Time to declare that this schedule is done (endCondition, endTime)
+// Flags to set (setFlag)
+ais.scheduled = function(who) {
+  DebugWrite("ai", "In SCHEDULED.");
+  delete who.flags.sleep;
+  // will be re-set in WaitHere if still asleep
+
+  var nextidx = who.getCurrentScheduleIndex() + 1;
   var schedule = DU.schedules[who.getSchedule()];
   if (nextidx >= schedule.scheduleArray.length) { nextidx = 0; }
-  
+
+  var currtime = GetUsableClockTime();
+
+  var nowactivity = schedule.scheduleArray[who.getCurrentScheduleIndex()];
   var nextactivity = schedule.scheduleArray[nextidx];
-  
+  if (nextactivity.params.startCondition === "Time") {
+    if (CheckTimeAfterTime(currtime, nextactivity.params.startTime)) {
+      DebugWrite("ai", "Moving to next scheduled activity- startTime met.");
+      who.setCurrentScheduleIndex(nextidx);
+    }
+  }
+  else if (nextactivity.params.startCondition === "PreviousComplete") {
+    if (nowactivity.params.endCondition === "Time") {
+      if (CheckTimeAfterTime(currtime, nowactivity.params.endTime)) {
+        DebugWrite("ai", "Moving to next scheduled activity- endTime met.");
+        who.setCurrentScheduleIndex(nextidx);
+      }
+    }
+    if (who.flags.activityComplete) {
+      who.setCurrentScheduleIndex(nextidx);
+      delete who.flags.activityComplete;
+    }
+  }
+
+  if (who.getCurrentScheduleIndex() === nextidx) { 
+    DebugWrite("ai", "Moving to next schedule, setting center for possible leash to current position.<br />");
+    who.leashCenter = {x: who.getx(), y: who.gety()}; 
+  }
+  nowactivity = schedule.scheduleArray[who.getCurrentScheduleIndex()];
+
+  if (nowactivity.params.bark) {
+    CheckNPCBark(who,nowactivity);
+  }
+
+  if (nowactivity.params.setFlag) {
+    if (nowactivity.params.setFlag.indexOf("unset_") !== -1) {
+      var tmpflag = nowactivity.params.set_flag.replace(/unset_/, "");
+      if (who.flags[tmpflag]) {
+        delete who.flags[tmpflag];
+      }
+    } else {
+      who.flags[nowactivity.params.set_flag] = 1;
+    }
+  }
+
+  return ais[nowactivity.type](who, nowactivity.params);
+}
+
+function CheckNPCBark(who,nowactivity) {
+  if (who.getHomeMap() === PC.getHomeMap()) { 
+    if (Dice.roll("1d100")  < nowactivity.params.barkfreq) {
+      if (GetDistance(who.getx(),who.gety(),PC.getx(),PC.gety()) <= nowactivity.params.barkrad) {
+        // bark!
+        DebugWrite("ai", "Townfolk barking.");
+        var barkarr = nowactivity.params.bark;
+        var idx = RollDice("1d" + barkarr.length + "-1");
+        var mybark = barkarr[idx];
+        if (mybark) {
+          if ((mybark.indexOf("%THEDESC%") !== -1) || (mybark.indexOf("%DESC%") !== -1)) {
+            var pref = who.getPrefix();
+            if (mybark.indexOf("%THEDESC%") !== -1) {
+              if ((pref === "a") || (pref === "an")) { pref = "the"; }
+            }
+            var desc = who.getDesc();
+            if (who.getDesc() !== who.getNPCName()) {
+              desc = pref + " " + desc;
+            }
+            mybark = mybark.replace(/%THEDESC%/g, desc);
+            mybark = mybark.replace(/%DESC%/g, desc);
+          }
+          mybark = mybark.charAt(0).toUpperCase() + mybark.slice(1);
+          maintext.addText(mybark);
+        }
+      }
+    }
+  }
 }
 
 // Called by the "Route To" schedule. Parameters include:
 // Destination (obj: x,y)
-// Bark information
-// Close doors behind
-// Time to declare that this schedule is done
-// Flags to set
-function AI_RouteTo(who, params) {
+// Close doors behind (closeDoors)
+ais.RouteTo = function(who, params) {
+  DebugWrite("ai", "In scheduled ai: RouteTo.");
 
+  if (params.closeDoors && (who.flags.closedoor.steps = 3)) {
+    var fea = who.getHomeMap().getTile(who.flags.closedoor.x,who.flags.closedoor.y).getTopFeature();
+    if (fea.closedgraphic) {
+      if (fea.open) {  // door hasn't been closed already
+        fea.use(who);
+        if (GetDistance(fea.getx(),fea.gety(),PC.getx(),PC.gety(),"square") <= 5) {
+          DrawMainFrame("draw",PC.getHomeMap(),PC.getx(),PC.gety());
+        } 
+        delete who.flags.closedoor;
+        DebugWrite("ai", "Turn spent closing a door.");
+        var retval = {};
+        retval["fin"] = 1;
+        return retval; 
+      } else {
+        delete who.flags.closedoor; // someone else closed the door
+      }
+    }
+  }
+
+  var path = whomap.getPath(who.getx(),who.gety(),params.destination.x,params.destination.y,who.getMovetype());
+  path.shift();
+  var moved = StepOrSidestep(who,path[0],[params.destination.x, params.destination.y]);
+  if (moved[opendoor]) {
+    who.flags.closedoor.steps = 1;
+    who.flags.closedoor.x = path[0][0];
+    who.flags.closedoor.y = path[0][1];
+  }
+
+  if ((who.getx() === params.destination.x) && (who.gety() === params.destination.y)) {
+    who.flags.activityComplete = 1;
+    DebugWrite("ai", "Arrived at destination, setting activityComplete.");
+  }
+  return moved;
+}
+
+ais.WaitHere = function(who,params) {
+  var whomap = who.getHomeMap();
+  DebugWrite("ai", "In WaitHere.");
+  if (params.sleep) { who.flags.sleep = 1; DebugWrite("ai", "ZZzzzz."); }
+  else {
+    if (params.responsibleFor) {
+      if (who.flags.closingResponsibleDoor >= 0) {
+        for (var i=0;i<params.responsibeFor.length;i++) {
+          var fea = who.getHomeMap().getTile(params.responsibleFor[i].x,params.responsibleFor[i].y).getTopFeature();
+          if (fea.open) {
+            if (RollDice("1d5") === 1) { 
+              DebugWrite("ai", "Chosen to close door at " + fea.getx() + "," + fea.gety() +".");
+              who.flags.closingResponsibleDoor = i; 
+            }
+          }
+        }
+      }
+      if (who.flags.closingResponsibleDoor) {
+        var door = who.flags.closingResponsibleDoor;
+        var fea = who.getHomeMap().getTile(params.responsibleFor[door].x,params.responsibleFor[door].y).getTopFeature();
+        if (fea.open) {
+          DebugWrite("ai", "Working on closing the door at " + fea.getx() + "," + fea.gety() +" as previously decided upon.");
+          if (IsAdjacent(who,fea,1)) {
+            DebugWrite("ai", "Closing the door.");             
+            fea.use(who);
+            if (GetDistance(fea.getx(),fea.gety(),PC.getx(),PC.gety(),"square") <= 5) {
+              DrawMainFrame("draw",whomap,PC.getx(),PC.gety());
+            }             
+            delete who.flags.closingResponsibleDoor;
+          } else {
+            DebugWrite("ai", "Approaching the door.");            
+            var path = whomap.getPath(who.getx(), who.gety(), params.responsibleFor[door].x, params.responsibleFor[door].y,MOVE_WALK_DOOR);
+            if (path.length === 0) {
+              // There is no path to the door, giving up on closing it... this time
+              DebugWrite("ai", "No path to the door, giving up.");
+              delete who.flags.closingResponsibleDoor;
+            } else {
+              path.shift();
+              StepOrSidestep(who, path[0], [params.responsibleFor[door].x, params.responsibleFor[door].y]);
+              DebugWrite("ai", "Moved towards the door.");
+            }
+          }
+        } else {
+          DebugWrite("ai", "Someone else has closed the door. Skipping my action out of confusion!");
+          delete who.flags.closingResponsibleDoor;
+        }
+      }
+    }
+
+    if (params.leashLength) {
+      if (GetDistance(who.getx(),who.gety(),who.leashCenter.x,who.leashCenter.y) > params.leashLength) {
+        var path = whomap.getPath(who.getx(),who.gety(),who.leashCenter.x,who.leashCenter.y,MOVE_WALK_DOOR);
+        if (path) {
+          path.shift();
+          StepOrSidestep(who,path[0], [who.leashCenter.x,who.leashCenter.y]);
+          DebugWrite("ai", "Tried to move toward the center of my leash.");
+        } else {
+          DebugWrite("ai", "No path back into my leash.");
+          // skipping turn in confusion
+        }
+      } else {
+        // I am permitted to wander
+        if (RollDice("1d2") === 2) {
+          var moveval = ais.Randomwalk(who,25,25,25,25);
+          DebugWrite("ai", "Wander wander wander.");
+        } else {
+          DebugWrite("ai", "Could have wandered, chose not to.");
+        }
+      }
+    }
+  }
+
+  return;
+}
+
+ais.LeaveMap = function(who,params) {
+  var whox = who.getx();
+  var whoy = who.gety();
+  who.getHomeMap().deleteThing(who);
+  DrawMainFrame("one",themap,whox,whoy); 
+  DebugWrite('ai', "I have left the map.");
+  DUTime.removeEntityFrom(who);  
+  return;
+}
+
+ais.ChangeMap = function(who,params) {
+  var desttile = MoveBetweenMaps(who,who.getHomeMap(),params.destination.mapName,params.destination.x,params.destination.y);
+  if (desttile) {
+    who.flags.activityComplete = 1;
+    DebugWrite("ai", "Changed maps.");
+  } else {
+    DebugWrite("ai", "Failed to change maps. Will try again next turn.");
+  }
+  return;
+}
+
+ais.CallAI = function(who,params) {
+  var retval = ais[params.ai_name](who,params);
+  if (retval["fin"] = 1) {
+    who.flags.activityComplete = 1;
+  }
+  return;
+}
+
+ais.PlaceItem = function(who,params) {
+  var item = localFactory.createTile(params.params.name);
+  if (item) {
+    who.getHomeMap().placeThing(params.params.x, params.params.y, item);
+    if (who.getHomeMap() === PC.getHomeMap()) {
+      DrawMainFrame("one",who.getHomeMap(),params.params.x,params.params.y);
+    }      
+  }
+  who.flags.activityComplete = 1;
+  who.linkedItem = item;
+
+  return;
+}
+
+ais.DeleteItem = function(who,params) {
+  if (params.params.param === "last") {
+    var item = who.linkedItem;
+    if (!item) {
+      DebugWrite("ai", "Trying to delete last item, cannot find. Marking complete...");
+      who.activityComplete = 1;
+    }
+    var itemx = item.getx();
+    var itemy = item.gety();
+    var itemmap = item.getHomeMap();
+
+    item.itemmap.deleteThing(item);
+
+    if (itemmap === PC.getHomeMap()) { DrawMainFrame("one",itemmap,itemx,itemy); }
+  }
+
+  return;
+}
+
+ais.PlayHarpsichord = function(who,params) {
+  // ?? Need to decide what this entails
+
+  return;
 }
