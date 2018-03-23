ee0290b13b559c7341bafc64af90b5016b8eb1e9
TapestryOfAges <gf@tapestryofages.com>
2018-03-22 23:50:09 -0700

    fixed test schedule

--- test/DUtests-Schedule.js
+++ test/DUtests-Schedule.js
@@ -64,7 +64,7 @@ QUnit.test( "Test randomwalk", function( assert ) {
   maps.addMap("unittest2");
   var testmap1 = maps.getMap("unittest2");
 
-  var sched = '{"testmage":{"scheduleArray":[{"params":{"destination":{"x":"8","y":"6"},"startCondition":"Time","startTime":"9:00"},"type":"RouteTo"},{"params":{"destination":{"mapName":"unittest2","x":"3","y":"10"},"startCondition":"PreviousComplete"},"type":"ChangeMap"},{"params":{"destination":{"x":"9","y":"10"},"closeDoors":"1","startCondition":"PreviousComplete"},"type":"RouteTo"},{"params":{"leashLength":"3","responsibleFor":[{"x":"6","y":"10"}],"startCondition":"PreviousComplete"},"type":"WaitHere"},{"params":{"AIName":"PlaceItem","params":"name: \\"RedPotion\\", x: 3, y:3","startCondition":"Time","startTime":"11:30"},"type":"CallAI"}],"baseLocation":"unittest","currentIndex":0}}';  
+  var sched = '{"testmage":{"scheduleArray":[{"params":{"destination":{"x":"8","y":"6"},"startCondition":"Time","startTime":"9:00"},"type":"RouteTo"},{"params":{"destination":{"mapName":"unittest2","x":"3","y":"10"},"startCondition":"PreviousComplete"},"type":"ChangeMap"},{"params":{"destination":{"x":"9","y":"10"},"closeDoors":"1","startCondition":"PreviousComplete"},"type":"RouteTo"},{"params":{"leashLength":"3","responsibleFor":[{"x":"6","y":"10"}],"startCondition":"PreviousComplete"},"type":"WaitHere"},{"params":{"AIName":"PlaceItem","params":"{\\"name\\":\\"RedPotion\\",\\"x\\":3,\\"y\\":3}","startCondition":"Time","startTime":"11:30"},"type":"CallAI"},{"params":{"leashLength":0,"startCondition":"PreviousComplete"},"type":"WaitHere"}],"baseLocation":"unittest","currentIndex":0}}'; 
   
   var tmpschedules = JSON.parse(sched);
   
