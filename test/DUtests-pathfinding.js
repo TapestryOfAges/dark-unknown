59eafdc0d62bed49031e96d3d0cf076a0407d61b
TapestryOfAges <gf@tapestryofages.com>
2018-05-13 23:16:48 -0700

    new

---
+++ test/DUtests-pathfinding.js
@@ -0,0 +1,26 @@
+"use strict;" 
+
+QUnit.test( "Test Pathfinder", function( assert ) {
+
+  var grid = new PF.Grid(12, 12);
+  var finder = new PF.AStarFinder();
+
+  grid.setWeightAt(0,3,1);
+  assert.deepEqual(grid.getWeightAt(1,4),1,"Default weight");
+
+  var gridBackup = grid.clone();
+
+  var path = finder.findPath(1, 7, 10, 2, gridBackup);
+
+  for (let i=0;i<path.length;i++) {
+    $("#output").append("[");
+
+    for (let j=0;j<path[i].length;j++) {
+      $("#output").append(path[i][j]);
+      if (!j) { $("#output").append(","); }
+    }
+    $("#output").append("],");
+  }
+
+});
+
