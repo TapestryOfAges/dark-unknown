cc11bada78d31d877b8c3b567e57e0c80ef28f84
TapestryOfAges <gf@tapestryofages.com>
2019-04-17 01:37:13 -0700

    a real start

--- npcSpecial.js
+++ npcSpecial.js
@@ -2,11 +2,19 @@
 
 // Introducing multi-tile sized NPC/Groups
 function MultiTileNPC(othertilearray,othertilelocationsarray) {
-  this.attachParts = function(partsarray) {
+  this.attachedParts = [];
+  this.attachParts = function() {
+    let mymap = this.getHomeMap();
+    for (let i=0; i<othertilearray.length; i++) {
+      let part = localFactory.createTile(othertilearray[i]);
+      this.attachedParts[this.attachedParts.length] = part;
 
+    }
   }
 }
 
-function MultiSegment() {
 
+function MultiSegment() {
+  this.attachedTo = {};
 }
+MultiSegment.prototype = new NPCObject();
