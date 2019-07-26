b658d3404b8b01427bfb6e84fb8d6014082e102b
TapestryOfAges <gf@tapestryofages.com>
2019-07-26 02:36:19 -0700

    fix

--- gameObjects.js
+++ gameObjects.js
@@ -10529,7 +10529,7 @@ function ToshinJournalTile() {
   this.desc = "journal";
   this.prefix = "a";
   this.contents = "You search through the journal for something useful.%%<span class='conv'>It is somewhere... I can taste it. It is close. The bones of Ellusus sing with it, the unblinking eye of a star.%%But there is something I need before I can use the Pool safely, even once I find it. Old scrolls call it an Infinite Scroll. I do not know how to make one... yet. But some are said to still exist. I will inquire of the black market.%%There is a guild of thieves in Onyx, but they also have knowledge of such things.<span>";
-  this.longdesc = "The Journals of Natassa.";
+  this.longdesc = "The Journals of Toshin.";
 }
 ToshinJournalTile.prototype = new BookItemObject();
 
