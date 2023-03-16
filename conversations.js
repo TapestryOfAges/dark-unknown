a59f4bf73153f8569a0d6893ec33f228645685c3
TapestryOfAges <gf@tapestryofages.com>
2023-03-15 20:33:28 -0700

    Finished convo standardization, _start and look don't show speaker info

--- conversations.js
+++ conversations.js
@@ -87,7 +87,7 @@ Conversation.prototype.respond = function(speaker, keyword, skipahead) {
   }
   
   let triggers = this[keyword].triggers[flags_met];
-  if (triggers.hasOwnProperty("altspeaker")) {
+  if (triggers.hasOwnProperty("altspeaker") || (keyword === "_start") || (keyword === "look")) {
     noshowmainspeaker = 1;
     console.log(noshowmainspeaker);
     if ((triggers.altspeaker !== 1) && (triggers.altspeaker !== "1")) { altspeaker = flags.altspeaker; }
