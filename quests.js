3f2ffb8ba7690ea81396a7630d3240e075d8726a
TapestryOfAges <gf@tapestryofages.com>
2024-01-18 14:09:00 -0800

    working on questlog

--- quests.js
+++ quests.js
@@ -1,9 +1,55 @@
 'use strict';
 
+class QuestLogEntry{
+  constructor(qnum) {
+    this.questnum = qnum;
+    this.active = 0;
+    this.completed = 0;
+    this.replaced = 0;
+  }
+}
+
+class Questlog {
+  constructor() {
+    this.log = [];
+  }
+
+  findQuest(questnum) {
+    for (let i=0;i<this.log.length;i++) {
+      if (this.log[i].questnum === questnum) { return i; }
+    }
+    return -1;
+  }
+
+  activate(questnum) {
+    let logentry = this.findQuest(questnum);
+    if (logentry === -1) {
+      let entry = new QuestLogEntry(questnum);
+      entry.active = 1;
+
+      if (quests[questnum].replaces) {
+        let repentry = this.findQuest(quests[questnum].replaces);
+        if (repentry !== -1) {
+          this.log[repentry].replaced = 1;
+        }
+      }
+    }
+  }
+
+  complete(questnum) {
+    let logentry = this.findQuest(questnum);
+    if (logentry === -1) {
+      console.log("Trying to complete quest " + questnum + " without having begun it.");
+    }
+  }
+
+}
+
 class Quest {
-  constructor(replaces, name, text, completetext, category) {
+  constructor(replaces, name, source, text, completetext, category) {
     this.replaces = replaces;
     this.name = name;
+    this.source = source;
     this.text = text;
     this.completetext = completetext;
     this.category = category;   // categories are main and side
@@ -11,25 +57,13 @@ class Quest {
     this.completed = 0;
   }
 
-  activate(log) {
-    if (this.getCompleted() < 1) {
-      for (let i=0;i<this.replaces.length;i++) {
-        log[this.replaces[i]].deactivate();
-        log[this.replaces[i]].complete("replaced");
-      }
-      this.active = 1;
-    }
-  }
-
-  deactivate() {
-    this.active = 0;
-  }
-
   getName() {
     return this.name;
   }
 
   getText() {
+    let txt = this.text;
+    
     return this.text;
   }
 
@@ -41,26 +75,17 @@ class Quest {
     return this.category;
   }
 
-  complete(replaced) {
-    this.deactivate();
-    if (replaced) { this.completed = -1; }
-    else {this.completed = 1;}
-  }
-
-  getCompleted() {
-    return this.completed;
-  }
 }
 
-let questlog = [];
+let questlist = [];
 
-questlog[0] = new Quest([], "Preventing a War", "Your brother is in rebellion. Find out why... and put a stop to it.", "You have saved your brother, and prevented a war.", "main");
-questlog[1] = new Quest([], "The Rune of Kings", "Journey to the Pit of Despair, and find the Rune of Kings.", "You have been marked by the Rune of Kings.", "main");
-questlog[2] = new Quest([], "To Earn a Spellbook", "Jharden needs an executioner's hood from the healer in Naurglen.", "You brought Jharden the executioner's hood he asked for.", "main");
-questlog[3] = new Quest([], "Nyrani's Shield", "Fetch Nyrani's shield from the armorsmith in Hildendain.", "You've brought Nyrani her shield.", "main");
-questlog[4] = new Quest([], "A Cave Near the Coast", "Trade is being threatened by monsters in a cave. Erin has asked you to see that something is done about it.", "The monsters in the cave are slain.", "main");
-questlog[5] = new Quest([], "Fall of Targion", "Olivia in Castle dea Olympus is looking for the book Fall of Targion, which is probably in the Tower of Toshin.", "You found the book for Olivia.", "side");
-questlog[6] = new Quest([], "Reports for the Chancellor", "Chancellor Tyler asked that you get reports from the leaders of Swainhil, Clear Lake, and Beldskae.", "You have brought the reports to the Chancellor.", "side");
+questlist[0] = new Quest([], "Preventing a War", "King Daragen", "Your brother is in rebellion. Find out why... and bring the threat to an end before it can start.", "You have saved your brother, and prevented a war.", "main");
+questlist[1] = new Quest([], "The Rune of Kings", "King Daragen", "Journey to the Pit of Despair, and find the Rune of Kings.", "You have been marked by the Rune of Kings.", "main");
+questlist[2] = new Quest([], "To Earn a Spellbook", "Jharden", "Jharden needs an executioner's hood from the healer in Naurglen.", "You brought Jharden the executioner's hood he asked for.", "main");
+questlist[3] = new Quest([], "Nyrani's Shield", "Nyrani", "Fetch Nyrani's shield from the armorsmith in Hildendain.", "You've brought Nyrani her shield.", "main");
+questlist[4] = new Quest([], "A Cave Near the Coast", "Erin", "Trade is being threatened by monsters in a cave. Erin has asked you to see that something is done about it.", "The monsters in the cave are slain.", "main");
+questlist[5] = new Quest([], "Fall of Targion", "Olivia", "Olivia in Castle dea Olympus is looking for the book Fall of Targion, which is probably in the Tower of Toshin.", "You found the book for Olivia.", "side");
+questlist[6] = new Quest([], "Reports for the Chancellor", "Tyler", "Chancellor Tyler asked that you get reports from the leaders of Swainhil, Clear Lake, and Beldskae.", "You have brought the reports to the Chancellor.", "side");
 
 // verbage for reference guide:
 // Not everything that you might think of as a "quest" will go in the log. And those that do, you will still need to take notes-
