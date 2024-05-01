04fb88980be871307f32a1fc5ec13b622442db8a
TapestryOfAges <gf@tapestryofages.com>
2024-05-01 00:27:23 -0700

    Nadya joins fight with black dragon

--- events.js
+++ events.js
@@ -128,6 +128,9 @@ EventFunctions["BDragon"] = function(ev) {
       }
       else if (npcs[i].getNPCName() === "Justice") { justice = npcs[i]; justice.setAttitude("neutral")}
       else if (npcs[i].getNPCName() === "Black Dragon") { dragon = npcs[i]; }
+      else if (npcs[i].getNPCName() === "Nadya") { 
+        if (npcs[i].gety() > 19) { npcs[i].setAttitude("neutral"); }  // Nadya joins the fight if in the throne room
+      }
       else {
         npcs[i].setAttitude("neutral"); // randos shouldn't be killed by the dragon
       }
