4411a809b236f50d5e907c84b2681c54e605c2c9
TapestryOfAges <gf@tapestryofages.com>
2025-05-13 13:57:05 -0700

    settings

--- e-preload.js
+++ e-preload.js
@@ -14,10 +14,11 @@ contextBridge.exposeInMainWorld('OutOfContext', {
   create_dir: () => { ipcRenderer.send('create_dir'); },
 
   write_save: (params) => { ipcRenderer.send('write_save', {idx: params[0], serialized: params[1]}); },
-  load_game: (idx) => {
-    ipcRenderer.send('load_save', idx);
-  },
+  load_game: (idx) => { ipcRenderer.send('load_save', idx); },
+  write_settings: (params) => { ipcRenderer.send('write_settings', params); },
+  load_settings: () => { ipcRenderer.send('load_settings'); },
   onGetSaveIndex: (callback) => { ipcRenderer.on('sendSaveIndex', callback) }, 
   onLoadData: (callback) => { ipcRenderer.on('sendLoad', callback) }, 
+  onLoadSettings: (callback) => { ipcRenderer.on('sendSettings', callback) },
 })
 
