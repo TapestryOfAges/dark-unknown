947e650680fc124917c2faeb448907a3bbf38460
TapestryOfAges <gf@tapestryofages.com>
2025-05-31 16:30:33 -0700

    when initiated into the paladins, remove karma penalties for their loot

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let eidos = new Platonic();
 //var universe = new Object;
 
 let DU = {};
-DU.version = "0.10.11";
+DU.version = "0.10.15";
 
 DU.PC = new PCObject();
 DU.gamelength = 0;
@@ -537,6 +537,7 @@ function DoAction(code, ctrl) {
         let newkey = localFactory.createTile("PaladinKey");
         PC.addToInventory(newkey,1);
         DU.gameflags.setFlag("paladin_joined",1);
+        RemoveKarmaPenaltyFromPaladins();
         questlog.complete(52);
         maintext.addText(`<span class='mainspeaker'>Isaac</span>: "The first thing I need to do now, is give you access to this tower. Here now is a key. What is in this tower is yours as well as ours, now- take anything you need."<br /><span class='sysconv'>You have obtained: ${newkey.getFullDesc()}.</span>`);
         targetCursor.step++;
