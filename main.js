320fd729ef63bcc97ca4ba8f82e9d9620a7f89f2
TapestryOfAges <gf@tapestryofages.com>
2026-05-15 15:01:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -326,7 +326,9 @@ function DoAction(code, ctrl) {
   }
   else if (gamestate.getMode() === "anykey") {
     if (targetCursor.event === "PlanarGate") {
-      
+      if (!targetCursor.frame) {
+        
+      }
     }
     if (targetCursor.command === "garrick") {
       let retval = GarrickScene(targetCursor.stage);
