5589c3e853c1436b524c5bb5876f5c1c3e40805c
TapestryOfAges <gf@tapestryofages.com>
2026-07-16 16:19:19 -0700

    work on tutorial

--- tutorial.js
+++ tutorial.js
@@ -7,7 +7,7 @@ mappages["tutorial1"].terrain = [];
  mappages["tutorial1"].terrain[3] = '.. .. .. .. .. .. .. ,, .. .. .. .. .. .. .. .. .. ,, .. ..';
  mappages["tutorial1"].terrain[4] = '.. .. u, .. .. .. .. .. .. .. .. .. .. ,, .. ,, ,, .. u, ..';
  mappages["tutorial1"].terrain[5] = '.. .. .. .. .. ,, .. .. .. .. .. .. .. .. u, ,, .. .. .. ..';
- mappages["tutorial1"].terrain[6] = 'u, .. .. ,, .. ## ## ## ## .. ## ## ## ## ## .. .. .. ,, ..';
+ mappages["tutorial1"].terrain[6] = 'u, .. .. ,, .. ## ## ## ## +c ## ## ## ## ## .. .. .. ,, ..';
  mappages["tutorial1"].terrain[7] = ',, .. ,, .. .. ## +c +c +c +c +c +c +c ## ## ,, .. ,, .. ..';
  mappages["tutorial1"].terrain[8] = '.. .. .. ,, .. ## +c +c +c +c +c +c +c ## ## .. ,, ,, .. ..';
  mappages["tutorial1"].terrain[9] = ',, ,, .. .. .. #O +c +c +c +c +c +c +c ## ## .. .. .. .. ..';
@@ -84,3 +84,74 @@ mappages["tutorial1"].linkedMaps = [""];
 mappages["tutorial1"].editorLabels = '{}';
 // MAP ENDS HERE
 
+// MAP BEGINS HERE
+mappages["tutorial2"] = {};
+mappages["tutorial2"].terrain = [];
+ mappages["tutorial2"].terrain[0] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[1] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[2] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[3] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[4] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[5] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+ mappages["tutorial2"].terrain[6] = 'sb sb sb sb sb ## ## ## ## ## ## #O ## ## ## sb sb sb sb sb';
+ mappages["tutorial2"].terrain[7] = 'sb sb sb sb sb ## +c +c +c ## +c +c +c ## ## sb sb sb sb sb';
+ mappages["tutorial2"].terrain[8] = 'sb sb sb sb sb ## +c +c +c ## +c +c +c ## ## sb sb sb sb sb';
+ mappages["tutorial2"].terrain[9] = 'sb sb sb sb sb #O +c +c +c ## +c +c +c ## ## sb sb sb sb sb';
+mappages["tutorial2"].terrain[10] = 'sb sb sb sb sb ## ## +c ## ## +c +c +c f# ## sb sb sb sb sb';
+mappages["tutorial2"].terrain[11] = 'sb sb sb sb sb ## +c +c +c ## +c +c +c ## ## sb sb sb sb sb';
+mappages["tutorial2"].terrain[12] = 'sb sb sb sb sb ## +c +c +c +c +c +c +c ## ## sb sb sb sb sb';
+mappages["tutorial2"].terrain[13] = 'sb sb sb sb sb ## ## ## ## ## ## #O ## ## ## sb sb sb sb sb';
+mappages["tutorial2"].terrain[14] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+mappages["tutorial2"].terrain[15] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+mappages["tutorial2"].terrain[16] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+mappages["tutorial2"].terrain[17] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+mappages["tutorial2"].terrain[18] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+mappages["tutorial2"].terrain[19] = 'sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb sb';
+
+mappages["tutorial2"].features = [];
+mappages["tutorial2"].features[0] = {name : 'Fireplace', x : 13, y : 10};
+mappages["tutorial2"].features[1] = {name : 'Door', x : 7, y : 10};
+mappages["tutorial2"].features[2] = {name : 'Door', x : 9, y : 12};
+mappages["tutorial2"].features[3] = {name : 'UnlitBrazier', x : 8, y : 9};
+mappages["tutorial2"].features[4] = {name : 'BedHead', x : 6, y : 7};
+mappages["tutorial2"].features[5] = {name : 'BedFoot', x : 7, y : 7};
+mappages["tutorial2"].features[6] = {name : 'Dresser', x : 8, y : 7};
+mappages["tutorial2"].features[7] = {name : 'BedHead', x : 11, y : 12};
+mappages["tutorial2"].features[8] = {name : 'BedFoot', x : 12, y : 12};
+mappages["tutorial2"].features[9] = {name : 'Dresser', x : 12, y : 9};
+mappages["tutorial2"].features[10] = {name : 'RightChair', x : 12, y : 10};
+mappages["tutorial2"].features[11] = {name : 'SmallTable', x : 11, y : 10};
+mappages["tutorial2"].features[12] = {name : 'Vanity', x : 12, y : 7};
+mappages["tutorial2"].features[13] = {name : 'Mirror', x : 10, y : 7};
+
+
+mappages["tutorial2"].npcs = [];
+
+mappages["tutorial2"].desc = "Tutorial";
+mappages["tutorial2"].longdesc = `Tutorial Map`;
+mappages["tutorial2"].music = 'Village';
+mappages["tutorial2"].savename = `tutorial`;
+mappages["tutorial2"].exitmap = '';
+mappages["tutorial2"].exitx = '65';
+mappages["tutorial2"].exity = '70';
+mappages["tutorial2"].wraps = '';
+mappages["tutorial2"].enterx = '65';
+mappages["tutorial2"].entery = '70';
+mappages["tutorial2"].seeBelow = '';
+mappages["tutorial2"].lightLevel = 'bright';
+mappages["tutorial2"].alwaysRemember = '0';
+mappages["tutorial2"].scale = '1';
+mappages["tutorial2"].underground = '0';
+mappages["tutorial2"].undergroundDesc = '';
+mappages["tutorial2"].enterscript = '';
+mappages["tutorial2"].entertestscript = '';
+mappages["tutorial2"].exitscript = '';
+mappages["tutorial2"].exittestscript = '';
+mappages["tutorial2"].returnmap = '';
+mappages["tutorial2"].returnx = 'NaN';
+mappages["tutorial2"].returny = 'NaN';
+mappages["tutorial2"].returninfused = '0';
+mappages["tutorial2"].automap = '1';
+mappages["tutorial2"].linkedMaps = [""];
+mappages["tutorial2"].editorLabels = '{}';
+// MAP ENDS HERE
